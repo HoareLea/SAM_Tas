0171de4cec4382d8a223413f4ec9c8c7ba28c2e7
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2019-12-04 15:38:05 +0000

    Tas initial implementation (WIP)

---
+++ SAM_Tas/SAM.Core.Tas/Import/ToT3D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM.Core.Tas
+{
+    public static class Import
+    {
+        public static bool ToT3D(string path_T3D, string path_gbXML)
+        {
+
+        }
+    }
+}
