cc2f3bb4bfa636c28be1e58c2d28f3754c93503c
Michal Dengusiak <tassimulation@gmail.com>
2020-01-23 09:07:09 +0000

    Update gbXMLTasT3D.cs

--- Grasshopper/SAM_Tas.Core.Grasshopper/Component/gbXMLTasT3D.cs
+++ Grasshopper/SAM_Tas.Core.Grasshopper/Component/gbXMLTasT3D.cs
@@ -152,7 +152,7 @@ namespace SAM.Core.Grasshopper
             {
                 //You can add image files to your project resources and access them like this:
                 // return Resources.IconForThisComponent;
-                return Resources.SAM_Small;
+                return Resources.SAM_TasT3D;
             }
         }
 
