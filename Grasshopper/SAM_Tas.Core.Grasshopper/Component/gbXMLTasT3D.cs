efdf9b47cd12ba5bec2fe1f8298efc78f41430dd
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-07-23 09:33:34 +0100

    gbXML To Tas Grasshopper Component Updated

--- Grasshopper/SAM_Tas.Core.Grasshopper/Component/gbXMLTasT3D.cs
+++ Grasshopper/SAM_Tas.Core.Grasshopper/Component/gbXMLTasT3D.cs
@@ -8,6 +8,16 @@ namespace SAM.Core.Grasshopper
     public class gbXMLTasT3D : GH_SAMComponent
     {
         /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("ae9cae75-6909-421f-8e68-f541703347b3");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_TasT3D;
+
+        /// <summary>
         /// Initializes a new instance of the SAM_point3D class.
         /// </summary>
         public gbXMLTasT3D()
@@ -16,18 +26,6 @@ namespace SAM.Core.Grasshopper
               "SAM", "Tas")
         {
         }
-
-        //    namespace SAM_TasCoreDynamo
-        //{
-        //    public static class T3D
-        //    {
-        //        public static bool BygbXML(string path_T3D, string path_gbXML, bool @override = true, bool fixNormals = true, bool zonesFromSpaces = true)
-        //        {
-        //            return SAM.Core.Tas.Import.ToT3D(path_T3D, path_gbXML, @override, fixNormals, zonesFromSpaces);
-        //        }
-
-        //    }
-        //}
 
         /// <summary>
         /// Registers all the input parameters for this component.
@@ -43,25 +41,6 @@ namespace SAM.Core.Grasshopper
             inputParamManager.AddBooleanParameter("_fixNormals_", "fixNormals", "bool Reverse wrong normals using Tas internal engine", GH_ParamAccess.item, true);
             inputParamManager.AddBooleanParameter("_zonesFromSpaces_", "zonesFromSpaces", "bool transforms Spaces for internal Tas Zones using Tas internal engine", GH_ParamAccess.item, true);
             inputParamManager.AddBooleanParameter("run_", "run_", "Connect Bool Toggle to run", GH_ParamAccess.item, false);
-
-            //aIndex = inputParamManager.AddBooleanParameter("_override_", "override", "bool override import setting for gbXML file", GH_ParamAccess.item, true);
-            //booleanParameter = (Param_Boolean)inputParamManager[aIndex];
-            // booleanParameter.PersistentData.Append(new GH_Boolean(true));
-
-            //aIndex = inputParamManager.AddBooleanParameter("_fixNormals_", "fixNormals", "bool Reverse wrong normals using Tas internal engine", GH_ParamAccess.item);
-            //booleanParameter = (Param_Boolean)inputParamManager[aIndex];
-            //booleanParameter.Optional = true;
-            //booleanParameter.PersistentData.Append(new GH_Boolean(true));
-
-            //aIndex = inputParamManager.AddBooleanParameter("_zonesFromSpaces_", "zonesFromSpaces", "bool transforms Spaces for internal Tas Zones using Tas internal engine", GH_ParamAccess.item);
-            //booleanParameter = (Param_Boolean)inputParamManager[aIndex];
-            //booleanParameter.Optional = true;
-            //booleanParameter.PersistentData.Append(new GH_Boolean(true));
-
-            //aIndex = inputParamManager.AddBooleanParameter("run_", "run_", "Connect Bool Toggle to run", GH_ParamAccess.item);
-            //booleanParameter = (Param_Boolean)inputParamManager[aIndex];
-            //booleanParameter.Optional = true;
-            //booleanParameter.PersistentData.Append(new GH_Boolean(false));
         }
 
         /// <summary>
@@ -69,7 +48,7 @@ namespace SAM.Core.Grasshopper
         /// </summary>
         protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
         {
-            outputParamManager.AddBooleanParameter("Success", "Sc", "Success", GH_ParamAccess.item);
+            outputParamManager.AddBooleanParameter("Successful", "Successful", "Correctly imported?", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -140,27 +119,6 @@ namespace SAM.Core.Grasshopper
             //IGeometry geometry = objectWrapper.Value as IGeometry;
 
             dataAccess.SetData(0, result);
-        }
-
-        /// <summary>
-        /// Provides an Icon for the component.
-        /// </summary>
-        protected override System.Drawing.Bitmap Icon
-        {
-            get
-            {
-                //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return Resources.SAM_TasT3D;
-            }
-        }
-
-        /// <summary>
-        /// Gets the unique ID for this component. Do not change this ID after release.
-        /// </summary>
-        public override Guid ComponentGuid
-        {
-            get { return new Guid("ae9cae75-6909-421f-8e68-f541703347b3"); }
         }
     }
 }
