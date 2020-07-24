faad9406d26289b369138432d314ab1cfdf43bd9
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-07-24 16:07:50 +0100

    T3D to TBD Export Implemented

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/TasT3DtoTBD.cs
@@ -0,0 +1,120 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Tas.Properties;
+using SAM.Core.Grasshopper;
+using System;
+
+namespace SAM.Analytical.Grasshopper.Tas
+{
+    public class TasT3DtoTBD : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("85b9fea2-d977-49c4-8295-8f207d10e13f");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_TasT3D;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public TasT3DtoTBD()
+          : base("Tas.T3DtoTBD", "Tas.T3DtoTBD",
+              "Exports/Converts Tas T3D to TBD",
+              "SAM", "Tas")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            //int aIndex = -1;
+            //Param_Boolean booleanParameter = null;
+
+            inputParamManager.AddTextParameter("_path_TasT3D", "pathTasT3D", "string path to TasT3D file", GH_ParamAccess.item);
+            inputParamManager.AddTextParameter("_path_TasTBD", "pathTasTBD", "string path to TasTBD file", GH_ParamAccess.item);
+            inputParamManager.AddIntegerParameter("_dayFirst_", "_dayFirst_", "First Day", GH_ParamAccess.item, 1);
+            inputParamManager.AddIntegerParameter("_dayLast_", "_dayLast_", "Last Day", GH_ParamAccess.item, 365);
+            inputParamManager.AddIntegerParameter("_step_", "_step_", "Day Step", GH_ParamAccess.item, 15);
+            inputParamManager.AddBooleanParameter("_autoAssignConstructions_", "_autoAssignConstructions_", "Auto Assign Constructions", GH_ParamAccess.item, true);
+            inputParamManager.AddBooleanParameter("run_", "run_", "Connect Bool Toggle to run", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddBooleanParameter("Successful", "Successful", "Correctly imported?", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            dataAccess.SetData(0, false);
+
+            bool run = false;
+            if (!dataAccess.GetData(6, ref run))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+            if (!run)
+                return;
+
+            string path_T3D = null;
+            if (!dataAccess.GetData(0, ref path_T3D) || string.IsNullOrWhiteSpace(path_T3D))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            string path_TBD = null;
+            if (!dataAccess.GetData(1, ref path_TBD) || string.IsNullOrWhiteSpace(path_TBD))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            int day_First = int.MinValue;
+            if (!dataAccess.GetData(2, ref day_First))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            int day_Last = int.MinValue;
+            if (!dataAccess.GetData(3, ref day_Last))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            int step = int.MinValue;
+            if (!dataAccess.GetData(4, ref step))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            bool autoAssignConstructions = true;
+            if (!dataAccess.GetData(4, ref autoAssignConstructions))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+
+            bool result = Analytical.Tas.Convert.ToTBD(path_T3D, path_TBD, day_First, day_Last, step, autoAssignConstructions);
+
+            dataAccess.SetData(0, result);
+        }
+    }
+}
---
+++ SAM_Tas/SAM.Analytical.Tas/Convert/ToTBD/TBD.cs
@@ -0,0 +1,42 @@
+﻿using SAM.Core.Tas;
+using TAS3D;
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Convert
+    {
+        public static bool ToTBD(this string path_T3D, string path_TBD, int day_First, int day_Last, int step, bool autoAssignConstructions)
+        {
+            if (string.IsNullOrWhiteSpace(path_T3D) || string.IsNullOrWhiteSpace(path_TBD))
+                return false;
+
+            bool result = false;
+            using (SAMT3DDocument sAMT3DDocument = new SAMT3DDocument(path_T3D))
+            {
+                result = ToTBD(sAMT3DDocument, path_TBD, day_First, day_Last, step, autoAssignConstructions);
+            }
+
+            return result;
+        }
+
+        public static bool ToTBD(this SAMT3DDocument sAMT3DDocument, string path_TBD, int day_First, int day_Last, int step, bool autoAssignConstructions)
+        {
+            if (sAMT3DDocument == null)
+                return false;
+
+            return ToTBD(sAMT3DDocument.T3DDocument, path_TBD, day_First, day_Last, step, autoAssignConstructions);
+        }
+
+        public static bool ToTBD(this T3DDocument t3DDocument, string path_TBD, int day_First, int day_Last, int step, bool autoAssignConstructions)
+        {
+            if (t3DDocument == null || string.IsNullOrWhiteSpace(path_TBD))
+                return false;
+
+            int int_autoAssignConstructions = 0;
+            if (autoAssignConstructions)
+                int_autoAssignConstructions = 1;
+
+            return t3DDocument.ExportNew(day_First, day_Last, step, 1, 1, 1, path_TBD, int_autoAssignConstructions, 0, 0);
+        }
+    }
+}
