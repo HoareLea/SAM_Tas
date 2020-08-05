f5ef789f711b852a99ffad82a9bbeb13efee3fe8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-08-05 09:09:40 +0100

    RemoveSchedules Functionality Implemented

---
+++ SAM_Tas/SAM.Analytical.Tas/Modify/RemoveSchedules.cs
@@ -0,0 +1,79 @@
+﻿using SAM.Core.Tas;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Modify
+    {
+        public static List<string> RemoveSchedules(this string path_TBD, string sufix, bool caseSensitive = true, bool trim = false)
+        {
+            if (string.IsNullOrWhiteSpace(path_TBD))
+                return null;
+
+            List<string> result = null;
+
+            using (SAMTBDDocument sAMTBDDocument = new SAMTBDDocument(path_TBD))
+            {
+                result = RemoveSchedules(sAMTBDDocument, sufix, caseSensitive, trim);
+                if (result != null)
+                    sAMTBDDocument.Save();
+            }
+
+            return result;
+        }
+
+        public static List<string> RemoveSchedules(this SAMTBDDocument sAMTBDDocument, string sufix, bool caseSensitive = true, bool trim = false)
+        {
+            if (sAMTBDDocument == null)
+                return null;
+
+            return RemoveSchedules(sAMTBDDocument.TBDDocument, sufix, caseSensitive, trim);
+        }
+
+        public static List<string> RemoveSchedules(this TBD.TBDDocument tBDDocument, string sufix, bool caseSensitive = true, bool trim = false)
+        {
+            if (tBDDocument == null || string.IsNullOrWhiteSpace(sufix))
+                return null;
+
+            TBD.Building building = tBDDocument.Building;
+            if (building == null)
+                return null;
+
+            List<string> result = new List<string>();
+
+            List<TBD.schedule> schedules = building.Schedules();
+            if (schedules == null || schedules.Count == 0)
+                return result;
+
+            string sufix_Temp = sufix;
+
+            if (trim)
+                sufix_Temp = sufix_Temp.Trim();
+
+            if (!caseSensitive)
+                sufix_Temp = sufix_Temp.ToUpper();
+
+            foreach (TBD.schedule schedule in schedules)
+            {
+                string name = schedule?.name;
+                if (string.IsNullOrEmpty(name))
+                    continue;
+
+                if (trim)
+                    name = name.Trim();
+
+                if (!caseSensitive)
+                    name = name.ToUpper();
+
+                if (name.EndsWith(sufix_Temp))
+                    result.Add(schedule.name);
+            }
+
+            foreach (string name in result)
+                tBDDocument.DeleteObjectByName(name);
+
+            return result;
+        }
+    }
+}
---
+++ SAM_Tas/SAM.Analytical.Tas/Query/Schedules.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Query
+    {
+        public static List<TBD.schedule> Schedules(this TBD.Building building)
+        {
+            List<TBD.schedule> result = new List<TBD.schedule>();
+
+            int index = 0;
+            TBD.schedule schedule = building.GetSchedule(index);
+            while (schedule != null)
+            {
+                result.Add(schedule);
+                index++;
+
+                schedule = building.GetSchedule(index);
+            }
+
+            return result;
+        }
+    }
+}
