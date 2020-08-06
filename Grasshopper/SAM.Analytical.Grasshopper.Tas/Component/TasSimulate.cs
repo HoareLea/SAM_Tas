f051ae31b0bdddef8b381b07f3e3ff7678575f04
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-08-06 12:02:40 +0100

    Simulate Method Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/TasSimulate.cs
@@ -0,0 +1,103 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Tas.Properties;
+using SAM.Core.Grasshopper;
+using System;
+
+namespace SAM.Analytical.Grasshopper.Tas
+{
+    public class TasSimulate : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("787e4fd5-d41c-4b71-b98a-fb20750362b5");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_TasT3D;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public TasSimulate()
+          : base("Tas.Simulate", "Tas.Simulate",
+              "Simulate TBD File",
+              "SAM", "Tas")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            //int aIndex = -1;
+            //Param_Boolean booleanParameter = null;
+
+            inputParamManager.AddTextParameter("_path_TasTBD", "pathTasTBD", "string path to TasTBD file", GH_ParamAccess.item);
+            inputParamManager.AddTextParameter("_path_TasTSD", "pathTasTSD", "string path to TasTSD file", GH_ParamAccess.item);
+            inputParamManager.AddIntegerParameter("_dayFirst_", "_dayFirst_", "First Day", GH_ParamAccess.item, 1);
+            inputParamManager.AddIntegerParameter("_dayLast_", "_dayLast_", "Last Day", GH_ParamAccess.item, 365);
+            inputParamManager.AddBooleanParameter("run_", "run_", "Connect Bool Toggle to run", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddBooleanParameter("Successful", "Successful", "Correctly imported?", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            dataAccess.SetData(1, false);
+
+            bool run = false;
+            if (!dataAccess.GetData(2, ref run))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+            if (!run)
+                return;
+
+            string path_TBD = null;
+            if (!dataAccess.GetData(0, ref path_TBD) || string.IsNullOrWhiteSpace(path_TBD))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            string path_TSD = null;
+            if (!dataAccess.GetData(1, ref path_TSD) || string.IsNullOrWhiteSpace(path_TSD))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            int day_First = -1;
+            if (!dataAccess.GetData(2, ref day_First))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            int day_Last = -1;
+            if (!dataAccess.GetData(3, ref day_Last))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            bool result = Analytical.Tas.Modify.Simulate(path_TBD, path_TSD, day_First, day_Last);
+
+            dataAccess.SetData(0, result);
+        }
+    }
+}
---
+++ SAM_Tas/SAM.Analytical.Tas/Modify/Simulate.cs
@@ -0,0 +1,35 @@
+﻿using SAM.Core.Tas;
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Modify
+    {
+        public static bool Simulate(string path_TBD, string path_TSD, int day_First, int day_Last)
+        {
+            if (string.IsNullOrWhiteSpace(path_TBD)  || string.IsNullOrWhiteSpace(path_TSD))
+                return false;
+
+            bool result = false;
+
+            using (SAMTBDDocument sAMTBDDocument = new SAMTBDDocument(path_TBD))
+            {
+                result = Simulate(sAMTBDDocument, path_TSD, day_First, day_Last);
+            }
+
+            return result;
+        }
+
+        public static bool Simulate(SAMTBDDocument sAMTBDDocument, string path_TSD, int day_First, int day_Last)
+        {
+            return Simulate(sAMTBDDocument?.TBDDocument, path_TSD, day_First, day_Last);
+        }
+
+        public static bool Simulate(TBD.TBDDocument tBDDocument, string path_TSD, int day_First, int day_Last)
+        {
+            if (tBDDocument == null || string.IsNullOrWhiteSpace(path_TSD))
+                return false;
+
+            return tBDDocument.simulate(day_First, day_Last, 0, 1, 0, 1, path_TSD, 1, 0) == 1;
+        }
+    }
+}
