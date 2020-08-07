dfc9127ba2fe34c680be55a05aa5fad0e778deeb
Michal Dengusiak <tassimulation@gmail.com>
2020-08-07 11:57:46 +0100

    add TBD icon

--- Grasshopper/SAM.Analytical.Grasshopper.Tas/Properties/Resources.Designer.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.Tas/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace SAM.Analytical.Grasshopper.Tas.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SAM_TasTBD {
+            get {
+                object obj = ResourceManager.GetObject("SAM_TasTBD", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
