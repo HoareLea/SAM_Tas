f88145462cf6356649dcb1284920ff43b3a6d5b2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-11-30 14:10:19 +0000

    AddMaterial Method Added

---
+++ SAM_Tas/SAM.Analytical.Tas/Modify/AddMaterial.cs
@@ -0,0 +1,19 @@
+﻿namespace SAM.Analytical.Tas
+{
+    public static partial class Modify
+    {
+        public static TBD.material AddMaterial(this TBD.Construction construction, SAM.Core.Material material)
+        {
+            if (construction == null || material == null)
+                return null;
+
+            TBD.material result = construction.AddMaterial();
+            if (result == null)
+                return result;
+
+            result.UpdateMaterial(material);
+
+            return result;
+        }
+  }
+}
