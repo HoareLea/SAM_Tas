6822ae07191518c58ac9cc9873f265f499cd504f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-01-19 09:02:50 +0000

    Comments Added for UpdateZones Method

--- SAM_Tas/SAM.Analytical.Tas/Modify/UpdateZones.cs
+++ SAM_Tas/SAM.Analytical.Tas/Modify/UpdateZones.cs
@@ -5,15 +5,25 @@ namespace SAM.Analytical.Tas
 {
     public static partial class Modify
     {
+        /// <summary>
+        /// Updates TBD.zones in given TBD.Buiding based on provided spaces and profileLibrary
+        /// </summary>
+        /// <param name="building">TBD.Buidling</param>
+        /// <param name="spaces">SAM Analytical Spaces</param>
+        /// <param name="profileLibrary">ProfileLibrary which contains information about profiles used in spaces</param>
+        /// <param name="includeHDD">Include Heating Design Day in the process</param>
+        /// <returns>TBD.zones have been used in update process</returns>
         public static bool UpdateZones(this TBD.Building building, IEnumerable<Space> spaces, ProfileLibrary profileLibrary, bool includeHDD = false)
         {
             if (building == null || spaces == null || profileLibrary == null)
                 return false;
 
+            //Zone Dictionary <- Dictionary constains zone.name as a key and TBD.zone as Value. Dictionary helps to match TBD.zone with SAM.Analytical.Space
             Dictionary<string, TBD.zone> dictionary_Zones = building.ZoneDictionary();
             if (dictionary_Zones == null)
                 return false;
-
+            
+            //Space Dictionary <- Dictionary constains Space.Name as a key and SAM.Analytical.Space as Value. Assumption: InternalCondition Name equals to Space Name. It also holds names for HDD Spaces/InternalConditions
             Dictionary<string, Space> dictionary_Spaces = new Dictionary<string, Space>();
             foreach(Space space in spaces)
             {
@@ -26,6 +36,7 @@ namespace SAM.Analytical.Tas
                     dictionary_Spaces[space.Name + " - HDD"] = space;
             }
 
+            //Removes Internal Conditions with given names. Names are taken from Space Name (assumption Space Name equals InternalCondtion Name)
             RemoveInternalConditions(building, dictionary_Spaces.Keys);
 
             List<TBD.zone> result = new List<TBD.zone>();
@@ -35,15 +46,20 @@ namespace SAM.Analytical.Tas
                 if (name == null)
                     continue;
 
+                //Matching Space with TBD.zone via name
                 TBD.zone zone = null;
                 if (!dictionary_Zones.TryGetValue(name, out zone) || zone == null)
                     continue;
 
+                //Update TBD.zone using data stored in space and ProfileLibrary
                 result.Add(building.UpdateZone(zone, space, profileLibrary));
+                
+                //Include HDD if includeHDD input set to true
                 if (includeHDD)
                     building.UpdateZone_HDD(zone, space, profileLibrary);
             }
 
+            //Updating Builidng Information
             building.description = string.Format("Delivered by SAM https://github.com/HoareLea/SAM [{0}]", System.DateTime.Now.ToString("yyyy/MM/dd"));
 
             TBD.GeneralDetails generaldetails = building.GetGeneralDetails();
@@ -61,6 +77,7 @@ namespace SAM.Analytical.Tas
                 generaldetails.TerrainType = TBD.TerrainType.tbdCity;
             }
 
+            //Returning TBD.zones have been used in update process
             return result != null && result.Count > 0;
         }
 
