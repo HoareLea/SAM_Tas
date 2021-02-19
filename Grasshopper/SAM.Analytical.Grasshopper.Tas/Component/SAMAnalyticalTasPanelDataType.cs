af5ce331b4f31d8f5f0e3380ccfe84ebb5080210
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-19 11:36:57 +0000

    GH_SAMEnumComponent Implemented

--- Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/SAMAnalyticalTasPanelDataType.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/SAMAnalyticalTasPanelDataType.cs
@@ -1,14 +1,11 @@
-﻿using GH_IO.Serialization;
-using Grasshopper.Kernel;
-using SAM.Analytical.Grasshopper.Tas.Properties;
+﻿using SAM.Analytical.Grasshopper.Tas.Properties;
 using SAM.Analytical.Tas;
 using SAM.Core.Grasshopper;
 using System;
-using System.Windows.Forms;
 
 namespace SAM.Analytical.Grasshopper.Tas
 {
-    public class SAMAnalyticalTasPanelDataType : GH_SAMComponent
+    public class SAMAnalyticalTasPanelDataType : GH_SAMEnumComponent<PanelDataType>
     {
         /// <summary>
         /// Gets the unique ID for this component. Do not change this ID after release.
@@ -18,80 +15,21 @@ namespace SAM.Analytical.Grasshopper.Tas
         /// <summary>
         /// The latest version of this component
         /// </summary>
-        public override string LatestComponentVersion => "1.0.0";
+        public override string LatestComponentVersion => "1.0.1";
 
         /// <summary>
         /// Provides an Icon for the component.
         /// </summary>
-        protected override System.Drawing.Bitmap Icon => Resources.SAM_TasTSD;
-
-        private PanelDataType panelDataType =  PanelDataType.Undefined;
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
 
         /// <summary>
-        /// Panel Type
+        /// Tas Panel Data Enum Component
         /// </summary>
         public SAMAnalyticalTasPanelDataType()
           : base("SAMAnalytical.PanelDataType", "SAMAnalytical.PanelDataType",
               "Select Panel Data Type",
               "SAM", "Tas")
         {
-        }
-
-        public override bool Write(GH_IWriter writer)
-        {
-            writer.SetInt32("PanelDataType", (int)panelDataType);
-            return base.Write(writer);
-        }
-
-        public override bool Read(GH_IReader reader)
-        {
-            int aIndex = -1;
-            if (reader.TryGetInt32("PanelDataType", ref aIndex))
-                panelDataType = (PanelDataType)aIndex;
-
-            return base.Read(reader);
-        }
-
-        protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu)
-        {
-            foreach (PanelDataType panelDataType in Enum.GetValues(typeof(PanelDataType)))
-                Menu_AppendItem(menu, panelDataType.ToString(), Menu_PanelTypeChanged, true, panelDataType == this.panelDataType).Tag = panelDataType;
-        }
-
-        private void Menu_PanelTypeChanged(object sender, EventArgs e)
-        {
-            if (sender is ToolStripMenuItem item && item.Tag is ApertureType)
-            {
-                //Do something with panelType
-                this.panelDataType = (PanelDataType)item.Tag;
-                ExpireSolution(true);
-            }
-        }
-
-        /// <summary>
-        /// Registers all the input parameters for this component.
-        /// </summary>
-        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
-        {
-        }
-
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
-        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
-        {
-            outputParamManager.AddGenericParameter("PanelDataType", "PanelDataType", "SAM Analytical PanelDataType", GH_ParamAccess.item);
-        }
-
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="dataAccess">
-        /// The DA object is used to retrieve from inputs and store in outputs.
-        /// </param>
-        protected override void SolveInstance(IGH_DataAccess dataAccess)
-        {
-            dataAccess.SetData(0, panelDataType);
         }
     }
 }
--- Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/SAMAnalyticalTasSpaceDataType.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/SAMAnalyticalTasSpaceDataType.cs
@@ -1,14 +1,11 @@
-﻿using GH_IO.Serialization;
-using Grasshopper.Kernel;
-using SAM.Analytical.Grasshopper.Tas.Properties;
+﻿using SAM.Analytical.Grasshopper.Tas.Properties;
 using SAM.Analytical.Tas;
 using SAM.Core.Grasshopper;
 using System;
-using System.Windows.Forms;
 
 namespace SAM.Analytical.Grasshopper.Tas
 {
-    public class SAMAnalyticalTasSpaceDataType : GH_SAMComponent
+    public class SAMAnalyticalTasSpaceDataType : GH_SAMEnumComponent<SpaceDataType>
     {
         /// <summary>
         /// Gets the unique ID for this component. Do not change this ID after release.
@@ -18,14 +15,12 @@ namespace SAM.Analytical.Grasshopper.Tas
         /// <summary>
         /// The latest version of this component
         /// </summary>
-        public override string LatestComponentVersion => "1.0.0";
+        public override string LatestComponentVersion => "1.0.1";
 
         /// <summary>
         /// Provides an Icon for the component.
         /// </summary>
-        protected override System.Drawing.Bitmap Icon => Resources.SAM_TasTSD;
-
-        private SpaceDataType spaceDataType = SpaceDataType.Undefined;
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
 
         /// <summary>
         /// Panel Type
@@ -35,63 +30,6 @@ namespace SAM.Analytical.Grasshopper.Tas
               "Select Space Data Type",
               "SAM", "Tas")
         {
-        }
-
-        public override bool Write(GH_IWriter writer)
-        {
-            writer.SetInt32("SpaceDataType", (int)spaceDataType);
-            return base.Write(writer);
-        }
-
-        public override bool Read(GH_IReader reader)
-        {
-            int aIndex = -1;
-            if (reader.TryGetInt32("SpaceDataType", ref aIndex))
-                spaceDataType = (SpaceDataType)aIndex;
-
-            return base.Read(reader);
-        }
-
-        protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu)
-        {
-            foreach (SpaceDataType spaceDataType in Enum.GetValues(typeof(SpaceDataType)))
-                Menu_AppendItem(menu, spaceDataType.ToString(), Menu_PanelTypeChanged, true, spaceDataType == this.spaceDataType).Tag = spaceDataType;
-        }
-
-        private void Menu_PanelTypeChanged(object sender, EventArgs e)
-        {
-            if (sender is ToolStripMenuItem item && item.Tag is ApertureType)
-            {
-                //Do something with panelType
-                this.spaceDataType = (SpaceDataType)item.Tag;
-                ExpireSolution(true);
-            }
-        }
-
-        /// <summary>
-        /// Registers all the input parameters for this component.
-        /// </summary>
-        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
-        {
-        }
-
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
-        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
-        {
-            outputParamManager.AddGenericParameter("SpaceDataType", "SpaceDataType", "SAM Analytical SpaceDataType", GH_ParamAccess.item);
-        }
-
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="dataAccess">
-        /// The DA object is used to retrieve from inputs and store in outputs.
-        /// </param>
-        protected override void SolveInstance(IGH_DataAccess dataAccess)
-        {
-            dataAccess.SetData(0, spaceDataType);
         }
     }
 }
