68dd38041c1d7f8a09ec064b6e80d45522264508
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-11-05 10:56:00 +0000

    T3D Window Conversion Added (WIP)

---
+++ SAM_Tas/SAM.Analytical.Tas/Convert/ToT3D/New/Window.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+using TAS3D;
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Convert
+    {
+        public static window ToT3D(this IOpening opening, Building building)
+        {
+            if (opening == null)
+                return null;
+
+            uint colour;
+            if (!Core.Query.TryGetValue(opening, "Color", out colour))
+            {
+                colour = Core.Convert.ToUint(Color.Black);
+            }
+
+            Geometry.Spatial.BoundingBox3D boundingBox3D = opening.Face3D?.GetBoundingBox();
+
+            double width = double.NaN;
+            if (!Core.Query.TryGetValue(opening, "Width", out width))
+            {
+                if(boundingBox3D == null)
+                {
+                    return null;
+                }
+
+            }
+
+            double height = double.NaN;
+            double level = double.NaN;
+
+            throw new System.NotImplementedException();
+
+
+            //    double height = double.NaN;
+            //double level = double.NaN;
+
+            //PlanarBoundary3D planarBoundary3D = opening.PlanarBoundary3D;
+            //if(planarBoundary3D == null)
+            //{
+            //    if (!Core.Query.TryGetValue(opening, "Width", out width))
+            //        return null;
+
+            //    if (!Core.Query.TryGetValue(opening, "Height", out height))
+            //        return null;
+
+            //    if (!Core.Query.TryGetValue(opening, "Level", out level))
+            //        return null;
+            //}
+            //else
+            //{
+            //    width = planarBoundary3D.Width();
+            //    height = planarBoundary3D.Height();
+            //    level = Analytical.Query.MinElevation(planarBoundary3D);
+            //}
+
+            window result = building.AddWindow(opening.Name, Query.OpeningType(opening), colour, height, width, level);
+
+            return result;
+        }
+    }
+}
---
+++ SAM_Tas/SAM.Analytical.Tas/Query/New/OpeningType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Query
+    {
+        public static int OpeningType(this IOpening opening)
+        {
+            throw new NotImplementedException();
+            
+            return 1;
+        }
+    }
+}
