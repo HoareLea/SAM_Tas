392c460572d98702386ea30f59f79c813b3d37d0
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-12-09 15:28:18 +0000

    HourlySurfaceResult Query Implemented

--- SAM_Tas/SAM.Analytical.Tas/Convert/ToSAM/PanelSimulationResult.cs
+++ SAM_Tas/SAM.Analytical.Tas/Convert/ToSAM/PanelSimulationResult.cs
@@ -16,97 +16,97 @@ namespace SAM.Analytical.Tas
 
             double value = double.NaN;
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.ApertureFlowIn, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.ApertureFlowIn, index, double.NaN);
             if(!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.ApertureFlowIn, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.ApertureFlowOut, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.ApertureFlowOut, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.ApertureFlowOut, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.ApertureOpening, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.ApertureOpening, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.ApertureOpening, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.ExternalCondensation, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.ExternalCondensation, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.ExternalCondensation, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.ExternalConduction, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.ExternalConduction, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.ExternalConduction, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.ExternalConvection, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.ExternalConvection, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.ExternalConvection, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.ExternalLongWave, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.ExternalLongWave, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.ExternalLongWave, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.ExternalSolarGain, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.ExternalSolarGain, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.ExternalSolarGain, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.ExternalTemperature, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.ExternalTemperature, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.ExternalTemperature, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.InternalCondensation, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.InternalCondensation, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.InternalCondensation, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.InternalConduction, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.InternalConduction, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.InternalConduction, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.InternalConvection, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.InternalConvection, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.InternalConvection, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.InternalLongWave, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.InternalLongWave, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.InternalLongWave, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.InternalSolarGain, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.InternalSolarGain, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.InternalSolarGain, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.InternalTemperature, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.InternalTemperature, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.InternalTemperature, value);
             }
 
-            value = surfaceData.AnnualSurfaceResult(PanelDataType.InterstitialCondensation, index, double.NaN);
+            value = surfaceData.HourlySurfaceResult(PanelDataType.InterstitialCondensation, index, double.NaN);
             if (!double.IsNaN(value))
             {
                 result.SetValue(Analytical.PanelSimulationResultParameter.InterstitialCondensation, value);
---
+++ SAM_Tas/SAM.Analytical.Tas/Query/HourlySurfaceResult.cs
@@ -0,0 +1,33 @@
+﻿using TSD;
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Query
+    { 
+        public static T HourlySurfaceResult<T>(this SurfaceData surfaceData, tsdSurfaceArray tsdSurfaceArray, int index, T @default = default)
+        {
+            if(surfaceData == null || index == -1)
+            {
+                return @default;
+            }
+
+            float value = surfaceData.GetHourlySurfaceResult(index, (int)tsdSurfaceArray);
+            if (!Core.Query.TryConvert(value, out T result))
+            {
+                return @default;
+            }
+
+            return result;
+        }
+
+        public static T HourlySurfaceResult<T>(this SurfaceData surfaceData, PanelDataType panelDataType, int index, T @default = default)
+        {
+            if (surfaceData == null || panelDataType == Tas.PanelDataType.Undefined)
+            {
+                return @default;
+            }
+
+            return HourlySurfaceResult<T>(surfaceData, panelDataType.TsdSurfaceArray().Value, index);
+        }
+    }
+}
