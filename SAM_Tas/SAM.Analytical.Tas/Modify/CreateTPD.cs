ea8e77918880cce25dd7fdef6189a6c585966bfc
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-01-28 16:09:41 +0000

    CreateTPD AHU Bug Fixes 6 (WIP)

--- SAM_Tas/SAM.Analytical.Tas/Modify/CreateTPD.cs
+++ SAM_Tas/SAM.Analytical.Tas/Modify/CreateTPD.cs
@@ -77,7 +77,7 @@ namespace SAM.Analytical.Tas
                         coolingGroup = plantRoom.AddCoolingGroup();
                         coolingGroup.Name = "Cooling Circuit Group";
                         coolingGroup.DesignPressureDrop = 17 + (circuitLength / 4);
-                        coolingGroup.SetPosition(offset.X + 200, offset.Y + 280);
+                        coolingGroup.SetPosition(offset.X + 230, offset.Y + 280);
                     }
 
                     //DHW Groups
@@ -843,7 +843,7 @@ namespace SAM.Analytical.Tas
             multiChiller.Duty.Type = TPD.tpdSizedVariable.tpdSizedVariableSize;
             multiChiller.Duty.SizeFraction = 1.0;
             multiChiller.Duty.AddDesignCondition(energyCentre.GetDesignCondition(2));
-            multiChiller.SetPosition(offset.X, offset.Y + 280);
+            multiChiller.SetPosition(offset.X + 100, offset.Y + 280);
 
             dynamic pump_Cooling = plantRoom.AddPump();
             pump_Cooling.Name = "Cooling Circuit Pump";
@@ -852,7 +852,7 @@ namespace SAM.Analytical.Tas
             pump_Cooling.OverallEfficiency.Value = 1;
             pump_Cooling.SetFuelSource(1, fuelSource_Electrical);
             pump_Cooling.Pressure = (multiChiller.DesignPressureDrop + coolingGroup.DesignPressureDrop) / 0.712;
-            pump_Cooling.SetPosition(offset.X + 100, offset.Y + 280);
+            pump_Cooling.SetPosition(offset.X - 80, offset.Y + 270);
 
             plantRoom.AddPipe(multiChiller, 1, pump_Cooling, 1);
             plantRoom.AddPipe(pump_Cooling, 1, coolingGroup, 1);
@@ -862,7 +862,7 @@ namespace SAM.Analytical.Tas
             plantController_Cooling.AddControlArc(pump_Cooling);
             dynamic plantSensorArc_Cooling = plantController_Cooling.AddSensorArcToComponent(coolingGroup, 1);
 
-            plantController_Cooling.SetPosition(offset.X + 180, offset.Y + 110 + 280);
+            plantController_Cooling.SetPosition(offset.X + 30, offset.Y + 110 + 220);
             plantController_Cooling.SensorArc1 = plantSensorArc_Cooling;
             SetWaterSideController(plantController_Cooling, WaterSideControllerSetup.Load, 0.1, 0.1);
             offset.X += 300;
