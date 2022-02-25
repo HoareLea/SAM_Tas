ddd7cc28d840c997cd8837a35589f29b5c883644
Michal Dengusiak <tassimulation@gmail.com>
2022-02-25 16:54:43 +0000

    Update WeatherYear.cs

--- SAM_Tas/SAM.Weather.Tas/Convert/ToSAM/WeatherYear.cs
+++ SAM_Tas/SAM.Weather.Tas/Convert/ToSAM/WeatherYear.cs
@@ -12,11 +12,11 @@ namespace SAM.Weather.Tas
             }
 
             Dictionary<WeatherDataType, List<double>> dictionary = new Dictionary<WeatherDataType, List<double>>();
-            dictionary[WeatherDataType.GlobalSolarRadiation] = weatherYear.AnnualParameter<double>(0);
-            dictionary[WeatherDataType.DiffuseSolarRadiation] = weatherYear.AnnualParameter<double>(1);
-            dictionary[WeatherDataType.CloudCover] = weatherYear.AnnualParameter<double>(2);
-            dictionary[WeatherDataType.DryBulbTemperature] = weatherYear.AnnualParameter<double>(3);
-            dictionary[WeatherDataType.RelativeHumidity] = weatherYear.AnnualParameter<double>(4);
+            dictionary[WeatherDataType.GlobalSolarRadiation] = weatherYear.AnnualParameter<double>(2);
+            dictionary[WeatherDataType.DiffuseSolarRadiation] = weatherYear.AnnualParameter<double>(3);
+            dictionary[WeatherDataType.CloudCover] = weatherYear.AnnualParameter<double>(4);
+            dictionary[WeatherDataType.DryBulbTemperature] = weatherYear.AnnualParameter<double>(0);
+            dictionary[WeatherDataType.RelativeHumidity] = weatherYear.AnnualParameter<double>(1);
             dictionary[WeatherDataType.WindSpeed] = weatherYear.AnnualParameter<double>(5);
             dictionary[WeatherDataType.WindDirection] = weatherYear.AnnualParameter<double>(6);
 
