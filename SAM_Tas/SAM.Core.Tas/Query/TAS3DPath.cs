64efb4caaa4ce7166941be9671116e7e940ac0ec
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-03-11 15:43:35 +0000

    Tas Paths Implemented

---
+++ SAM_Tas/SAM.Core.Tas/Query/TAS3DPath.cs
@@ -0,0 +1,16 @@
+﻿namespace SAM.Core.Tas
+{
+    public static partial class Query
+    {
+        public static string TAS3DPath()
+        {
+            string tasDirectory = TasDirectory();
+            if(string.IsNullOrWhiteSpace(tasDirectory))
+            {
+                return null;
+            }
+
+            return System.IO.Path.Combine(tasDirectory, "TAS3D.exe");
+        }
+    }
+}
---
+++ SAM_Tas/SAM.Core.Tas/Query/TasDirectory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Win32;
+
+namespace SAM.Core.Tas
+{
+    public static partial class Query
+    {
+        public static string TasDirectory()
+        {
+            return Registry.GetValue(@"HKEY_CURRENT_USER\Software\EDSL\TasManager\TasFiles", "Path", null) as string;
+        }
+    }
+}
