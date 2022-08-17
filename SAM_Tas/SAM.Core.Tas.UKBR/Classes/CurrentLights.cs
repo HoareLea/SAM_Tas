afc9ed910fd0b999a98762527f312125d10287f2
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-08-17 17:07:57 +0100

    CurrentLights Implemented

---
+++ SAM_Tas/SAM.Core.Tas.UKBR/Classes/CurrentLights.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace SAM.Core.Tas.UKBR
+{
+    public class CurrentLights : UKBRElement
+    {
+        public override string UKBRName => "CurrentLights";
+
+        public CurrentLights(XElement xElement)
+            : base(xElement)
+        {
+
+        }
+
+        public bool bBackSpaceSensor
+        {
+            get
+            {
+                return Query.Value<bool>(xElement?.Attribute("bBackSpaceSensor"));
+            }
+        }
+
+        public bool bEfficacyLightingFunc
+        {
+            get
+            {
+                return Query.Value<bool>(xElement?.Attribute("bEfficacyLightingFunc"));
+            }
+        }
+
+        public double LampEfficiencyPer100Lux
+        {
+            get
+            {
+                return Query.Value<double>(xElement?.Attribute("LampEfficiencyPer100Lux"));
+            }
+        }
+
+        public double LampGeneralPowerDensity
+        {
+            get
+            {
+                return Query.Value<double>(xElement?.Attribute("LampGeneralPowerDensity"));
+            }
+        }
+
+        public double LampEfficacy
+        {
+            get
+            {
+                return Query.Value<double>(xElement?.Attribute("LampEfficacy"));
+            }
+        }
+
+        public double LampType
+        {
+            get
+            {
+                return Query.Value<double>(xElement?.Attribute("LampType"));
+            }
+        }
+    }
+}
