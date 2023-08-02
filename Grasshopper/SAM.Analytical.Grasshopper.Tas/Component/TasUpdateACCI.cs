8eda048ae87210a3641f885072bcda3dbb0f07fb
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-08-02 12:03:22 +0200

    Update ACCI Implemented

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/TasUpdateACCI.cs
@@ -0,0 +1,86 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Tas.Properties;
+using SAM.Core.Grasshopper;
+using System;
+
+namespace SAM.Analytical.Grasshopper.Tas
+{
+    public class TasUpdateACCI : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("12c19e3e-6cf1-4112-b7a4-0c4bcaa408e2");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_TasTBD;
+
+        public override GH_Exposure Exposure => GH_Exposure.tertiary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public TasUpdateACCI()
+          : base("Tas.UpdateACCI", "Tas.UpdateACCI",
+              "Updates ACCI.",
+              "SAM", "Tas")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            //int aIndex = -1;
+            //Param_Boolean booleanParameter = null;
+
+            inputParamManager.AddTextParameter("_pathTasTBD", "pathTasTBD", "The string path to TasTBD file.", GH_ParamAccess.item);
+            inputParamManager.AddBooleanParameter("_run", "_run", "Connect a boolean toggle to run.", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddBooleanParameter("successful", "successful", "Correctly imported?", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            dataAccess.SetData(1, false);
+
+            bool run = false;
+            if (!dataAccess.GetData(2, ref run))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+            if (!run)
+                return;
+
+            string path_TBD = null;
+            if (!dataAccess.GetData(0, ref path_TBD) || string.IsNullOrWhiteSpace(path_TBD))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            bool result = Analytical.Tas.Modify.UpdateACCI(path_TBD);
+
+            dataAccess.SetData(0, result);
+        }
+    }
+}
---
+++ SAM_Tas/SAM.Analytical.Tas/Modify/UpdateACCI.cs
@@ -0,0 +1,95 @@
+﻿using SAM.Core;
+using SAM.Core.Tas;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Modify
+    {
+        public static bool UpdateACCI(this TBD.Building building)
+        {
+            if (building == null)
+            {
+                return false;
+            }
+
+            List<TBD.InternalCondition> internalConditions = building.InternalConditions();
+            if(internalConditions == null || internalConditions.Count == 0)
+            {
+                return false;
+            }
+
+
+            List<double> dryBulbTemperatures = Weather.Tas.Query.AnnualParameter<double>(building.GetWeatherYear(), Weather.WeatherDataType.DryBulbTemperature);
+            if(dryBulbTemperatures == null || dryBulbTemperatures.Count != 8760)
+            {
+                return false;
+            }
+
+            bool result = false;
+            foreach (TBD.InternalCondition internalCondition in internalConditions)
+            {
+                TBD.profile profile_UpperLimit = internalCondition.GetThermostat().GetProfile((int)TBD.Profiles.ticUL);
+                if (profile_UpperLimit == null)
+                {
+                    continue;
+                }
+
+                TBD.profile profile_LowerLimit = internalCondition.GetThermostat().GetProfile((int)TBD.Profiles.ticLL);
+                if (profile_LowerLimit == null)
+                {
+                    continue;
+                }
+
+                profile_UpperLimit.type = TBD.ProfileTypes.ticYearlyProfile;
+                profile_LowerLimit.type = TBD.ProfileTypes.ticYearlyProfile;
+
+                for (int i = 1; i <= 8760; i++)
+                {
+                    Range<double> dryBulbTemperatureRange = Weather.Query.DryBulbTemperatureRange(dryBulbTemperatures[i]);
+                    if(dryBulbTemperatureRange == null)
+                    {
+                        continue;
+                    }
+
+                    profile_UpperLimit.yearlyValues[i] = System.Convert.ToSingle(dryBulbTemperatureRange.Max);
+                    profile_LowerLimit.yearlyValues[i] = System.Convert.ToSingle(dryBulbTemperatureRange.Min);
+                    result = true;
+                }
+
+            }
+
+            return result;
+        }
+
+        public static bool UpdateACCI(this SAMTBDDocument sAMTBDDocument)
+        {
+            if (sAMTBDDocument == null)
+                return false;
+
+            return UpdateACCI(sAMTBDDocument.TBDDocument?.Building);
+        }
+
+        public static bool UpdateACCI(string path_TBD)
+        {
+            if (string.IsNullOrWhiteSpace(path_TBD))
+            {
+                return false;
+            }
+
+            bool result = false;
+
+            using (SAMTBDDocument sAMTBDDocument = new SAMTBDDocument(path_TBD))
+            {
+                result = UpdateACCI(sAMTBDDocument);
+                if (result)
+                {
+                    sAMTBDDocument.Save();
+                }
+            }
+
+            return result;
+        }
+    }
+}
