71732de4b99e17c7c33d3152252aba546f021d31
michaldengusiak <tassimulation@gmail.com>
2023-09-08 16:28:07 +0100

    Update TasWorkflowgbXML.cs

--- Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/TasWorkflowgbXML.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/TasWorkflowgbXML.cs
@@ -19,7 +19,7 @@ namespace SAM.Analytical.Grasshopper.Tas
         /// <summary>
         /// The latest version of this component
         /// </summary>
-        public override string LatestComponentVersion => "1.0.3";
+        public override string LatestComponentVersion => "1.0.4";
 
         /// <summary>
         /// Provides an Icon for the component.
