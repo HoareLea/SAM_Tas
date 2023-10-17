dfc8f4ed8662fafe191857ab945797cf9a65f1e3
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-10-17 12:17:58 +0200

    UKBRStudio2021Path Query Added

---
+++ SAM_Tas/SAM.Core.Tas/Query/UKBRStudio2021Path.cs
@@ -0,0 +1,16 @@
+﻿namespace SAM.Core.Tas
+{
+    public static partial class Query
+    {
+        public static string UKBRStudio2021Path()
+        {
+            string tasDirectory = TasDirectory();
+            if(string.IsNullOrWhiteSpace(tasDirectory))
+            {
+                return null;
+            }
+
+            return System.IO.Path.Combine(tasDirectory, "UKBRStudio2021.exe");
+        }
+    }
+}
