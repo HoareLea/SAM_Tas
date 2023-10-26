d18f409519c05224c791441c7acf784378072b89
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-10-26 17:43:35 +0200

    Material Query Added for LayerThicknessCalculationResult and LayerThicknessCalculationData

---
+++ SAM_Tas/SAM.Analytical.Tas/Query/Material.cs
@@ -0,0 +1,75 @@
+﻿using SAM.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Query
+    {
+        public static IMaterial Material(this LayerThicknessCalculationResult layerThicknessCalculationResult, ConstructionManager constructionManager)
+        {
+            if(layerThicknessCalculationResult == null || constructionManager == null)
+            {
+                return null;
+            }
+
+            Construction construction = constructionManager.GetConstructions(layerThicknessCalculationResult.ConstructionName)?.FirstOrDefault();
+            if(construction == null)
+            {
+                return null;
+            }
+
+            List<ConstructionLayer> constructionLayers = construction.ConstructionLayers;
+            if(constructionLayers == null || constructionLayers.Count == 0)
+            {
+                return null;
+            }
+
+            if(constructionLayers.Count <= layerThicknessCalculationResult.LayerIndex)
+            {
+                return null;
+            }
+
+            string name = constructionLayers[layerThicknessCalculationResult.LayerIndex]?.Name;
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                return null;
+            }
+
+            return constructionManager.GetMaterial(name);
+        }
+
+        public static IMaterial Material(this LayerThicknessCalculationData layerThicknessCalculationData, ConstructionManager constructionManager)
+        {
+            if (layerThicknessCalculationData == null || constructionManager == null)
+            {
+                return null;
+            }
+
+            Construction construction = constructionManager.GetConstructions(layerThicknessCalculationData.ConstructionName)?.FirstOrDefault();
+            if (construction == null)
+            {
+                return null;
+            }
+
+            List<ConstructionLayer> constructionLayers = construction.ConstructionLayers;
+            if (constructionLayers == null || constructionLayers.Count == 0)
+            {
+                return null;
+            }
+
+            if (constructionLayers.Count <= layerThicknessCalculationData.LayerIndex)
+            {
+                return null;
+            }
+
+            string name = constructionLayers[layerThicknessCalculationData.LayerIndex]?.Name;
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return null;
+            }
+
+            return constructionManager.GetMaterial(name);
+        }
+    }
+}
