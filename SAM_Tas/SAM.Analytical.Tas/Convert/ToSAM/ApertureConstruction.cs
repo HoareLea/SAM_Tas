d4d2fed8bc5ca7e8a9be7b2e1f05874081cf2f74
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-11-10 09:25:57 +0100

    ApertureConstruction from TCD Conversion Added

--- SAM_Tas/SAM.Analytical.Tas/Convert/ToSAM/ApertureConstruction.cs
+++ SAM_Tas/SAM.Analytical.Tas/Convert/ToSAM/ApertureConstruction.cs
@@ -43,5 +43,45 @@ namespace SAM.Analytical.Tas
             ApertureConstruction result = new ApertureConstruction(System.Guid.NewGuid(), name, apertureType, constructionLayers_Pane, constructionLayers_Frame);
             return result;
         }
+
+        public static ApertureConstruction ToSAM_ApertureConstruction(this TCD.Construction construction, ApertureType apertureType)
+        {
+            if (construction == null)
+            {
+                return null;
+            }
+
+            List<ConstructionLayer> constructionLayers = ToSAM_ConstructionLayers(construction);
+
+            List<ConstructionLayer> constructionLayers_Pane = null;
+            List<ConstructionLayer> constructionLayers_Frame = null;
+
+            string name = construction.name?.Trim();
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                if (name.EndsWith(AperturePart.Frame.Sufix()))
+                {
+                    name = name.Substring(0, name.Length - AperturePart.Frame.Sufix().Length - 1).Trim();
+                    constructionLayers_Frame = constructionLayers;
+
+                }
+                else if (name.EndsWith(AperturePart.Pane.Sufix()))
+                {
+                    name = name.Substring(0, name.Length - AperturePart.Pane.Sufix().Length - 1).Trim();
+                    constructionLayers_Pane = constructionLayers;
+                }
+                else
+                {
+                    constructionLayers_Pane = constructionLayers;
+                }
+            }
+            else
+            {
+                constructionLayers_Pane = constructionLayers;
+            }
+
+            ApertureConstruction result = new ApertureConstruction(System.Guid.NewGuid(), name, apertureType, constructionLayers_Pane, constructionLayers_Frame);
+            return result;
+        }
     }
 }
