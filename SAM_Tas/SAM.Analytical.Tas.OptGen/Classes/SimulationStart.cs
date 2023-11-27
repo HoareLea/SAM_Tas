463ed05020519a4cd63f312ad879e855ded422cd
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-11-27 09:28:01 +0100

    SimulationStart Command Updated

--- SAM_Tas/SAM.Analytical.Tas.OptGen/Classes/SimulationStart.cs
+++ SAM_Tas/SAM.Analytical.Tas.OptGen/Classes/SimulationStart.cs
@@ -3,8 +3,8 @@
     [Attributes.Name("SimulationStart")]
     public class SimulationStart : OptGenObject
     {
-        [Attributes.Name("Command")]
-        public string Command { get; set; } = "cmd /c \"start  /WAIT /MIN \"\" \"C:\\Program Files\\Environmental Design Solutions Ltd\\Tas\\TasGenOpt\\TasGenExecute.exe\" \"C:\\Users\\DengusiakM\\Desktop\\SAM\\2023-11-21_GenOpt1\" ";
+        [Attributes.Name("Command"), Attributes.QuotedValue()]
+        public string Command { get; set; } = "cmd /c \\\"start  /WAIT /MIN \\\"\\\" \\\"C:\\\\Program Files\\\\Environmental Design Solutions Ltd\\\\Tas\\\\TasGenOpt\\\\TasGenExecute.exe\\\" \\\"C:\\\\Users\\\\DengusiakM\\\\Desktop\\\\SAM\\\\2023-11-21_GenOpt1\\\" ";
 
         [Attributes.Name("WriteInputFileExtension")]
         public bool WriteInputFileExtension { get; set; } = true;
