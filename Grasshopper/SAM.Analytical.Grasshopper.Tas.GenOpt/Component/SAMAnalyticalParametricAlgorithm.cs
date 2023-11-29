dca706e8b3fd3f40f2a5c9263280b933b7fc1fc1
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-11-29 17:10:24 +0100

    ParametricAlgorithm Implemented

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Tas.GenOpt/Component/SAMAnalyticalParametricAlgorithm.cs
@@ -0,0 +1,93 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using SAM.Analytical.Grasshopper.Tas.GenOpt.Properties;
+using SAM.Analytical.Tas.GenOpt;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper.Tas.GenOpt
+{
+    public class SAMAnalyticalParametricAlgorithm : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("52311cea-aab1-4448-8fd8-e87e2458bf10");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_GenOpt;
+
+
+        public override GH_Exposure Exposure => GH_Exposure.tertiary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalParametricAlgorithm()
+          : base("SAMAnalytical.ParametricAlgorithm", "SAMAnalytical.ParametricAlgorithm",
+              "SAM Analytical GenOpt Parametric Algorithm",
+              "SAM WIP", "Tas")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                Param_Boolean @boolean = new Param_Boolean() { Name = "_stopAtError_", NickName = "_stopAtError_", Description = "Stop At Error", Optional = true, Access = GH_ParamAccess.item };
+                @boolean.SetPersistentData(false);
+                result.Add(new GH_SAMParam(@boolean, ParamVisibility.Binding));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAlgorithmParam() { Name = "algorithm", NickName = "algorithm", Description = "Algorithm", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            bool stopAtError = false;
+            index = Params.IndexOfInputParam("_stopAtError_");
+            if (index == -1 || !dataAccess.GetData(index, ref stopAtError))
+            {
+                stopAtError = false;
+            }
+
+            index = Params.IndexOfOutputParam("algorithm");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, new ParametricAlgorithm() { StopAtError = stopAtError});
+            }
+        }
+    }
+}
---
+++ SAM_Tas/SAM.Analytical.Tas.GenOpt/Classes/ParametricAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿namespace SAM.Analytical.Tas.GenOpt
+{
+    public class ParametricAlgorithm : Algorithm
+    {
+        [Attributes.Name("StopAtError")]
+        public bool StopAtError { get; set; } = false;
+
+        [Attributes.Name("Main")]
+        public override AlgorithmType AlgorithmType { get; } = AlgorithmType.Parametric;
+    }
+}
