a4b36c27b8b703a853e6539ccd9db9c10ce4b60f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-01-15 10:32:19 +0100

    SpaceDataType Updated

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemSpaceResult.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemSpaceResult.cs
@@ -10,7 +10,7 @@ namespace SAM.Analytical.Tas.TPD
 {
     public static partial class Convert
     {
-        public static SystemSpaceResult ToSAM_SpaceSystemResult(this SystemZone systemZone, SystemPlantRoom systemPlantRoom, int start, int end, params SystemSpaceDataType[] systemSpaceDataTypes)
+        public static SystemSpaceResult ToSAM_SpaceSystemResult(this SystemZone systemZone, SystemPlantRoom systemPlantRoom, int start, int end, params SpaceDataType[] systemSpaceDataTypes)
         {
             if (systemZone == null)
             {
@@ -25,23 +25,23 @@ namespace SAM.Analytical.Tas.TPD
                 return null;
             }
 
-            IEnumerable<SystemSpaceDataType> systemSpaceDataTypes_Temp = systemSpaceDataTypes == null || systemSpaceDataTypes.Length == 0 ? Enum.GetValues(typeof(SystemSpaceDataType)).Cast<SystemSpaceDataType>() : systemSpaceDataTypes;
+            IEnumerable<SpaceDataType> systemSpaceDataTypes_Temp = systemSpaceDataTypes == null || systemSpaceDataTypes.Length == 0 ? Enum.GetValues(typeof(SpaceDataType)).Cast<SpaceDataType>() : systemSpaceDataTypes;
 
-            Dictionary<SystemSpaceDataType, IndexedDoubles> dictionary = new Dictionary<SystemSpaceDataType, IndexedDoubles>();
-            foreach (SystemSpaceDataType systemSpaceDataType in systemSpaceDataTypes_Temp)
+            Dictionary<SpaceDataType, IndexedDoubles> dictionary = new Dictionary<SpaceDataType, IndexedDoubles>();
+            foreach (SpaceDataType spaceDataType in systemSpaceDataTypes_Temp)
             {
 
                 //object @object = ((global::TPD.SystemComponent)systemZone as dynamic).GetResultsData(tpdResultsPeriod.tpdResultsPeriodHourly, tpdCombinerType.tpdCombinerTypeMax, 2, 1, 8760);
 
-                IndexedDoubles indexedDoubles = Create.IndexedDoubles((global::TPD.SystemComponent)systemZone, systemSpaceDataType, start, end);
+                IndexedDoubles indexedDoubles = Create.IndexedDoubles((global::TPD.SystemComponent)systemZone, spaceDataType, start, end);
                 if (indexedDoubles == null)
                 {
                     continue;
                 }
 
-                if (!dictionary.TryGetValue(systemSpaceDataType, out IndexedDoubles indexedDoubles_Temp) || indexedDoubles_Temp == null)
+                if (!dictionary.TryGetValue(spaceDataType, out IndexedDoubles indexedDoubles_Temp) || indexedDoubles_Temp == null)
                 {
-                    dictionary[systemSpaceDataType] = indexedDoubles;
+                    dictionary[spaceDataType] = indexedDoubles;
                 }
                 else
                 {
