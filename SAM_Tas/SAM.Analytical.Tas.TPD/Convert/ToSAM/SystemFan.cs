cad791e99e1d7d95f8a0aaa54d5d175ce56fffa1
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-02-28 16:55:08 +0100

    SystemCoolingCoil and SystemFan Conversion Updated

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemFan.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemFan.cs
@@ -1,5 +1,6 @@
 ﻿using TPD;
 using SAM.Analytical.Systems;
+using SAM.Geometry.Planar;
 
 namespace SAM.Analytical.Tas.TPD
 {
@@ -17,6 +18,10 @@ namespace SAM.Analytical.Tas.TPD
             SystemFan result = new SystemFan(@dynamic.Name);
             result.Description = dynamic.Description;
             Modify.SetReference(result, @dynamic.GUID);
+
+            Point2D location = ((TasPosition)@dynamic.GetPosition())?.ToSAM();
+
+            result = Systems.Create.DisplayObject<DisplaySystemFan>(result, location, Systems.Query.DefaultDisplaySystemManager());
 
             return result;
         }
