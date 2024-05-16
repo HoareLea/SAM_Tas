e7ecfb59a4eb4869f9eba385209be4ec22a0459a
michaldengusiak <tassimulation@gmail.com>
2024-05-16 16:58:50 +0100

    Update UpdateConstruction.cs

--- SAM_Tas/SAM.Analytical.Tas/Modify/UpdateConstruction.cs
+++ SAM_Tas/SAM.Analytical.Tas/Modify/UpdateConstruction.cs
@@ -15,18 +15,20 @@ namespace SAM.Analytical.Tas
 
             bool update = true;
 
-            if (constructionLayers_TBD != null && constructionLayers != null && constructionLayers_TBD.Count == constructionLayers.Count())
-            {
-                update = false;
-                for (int i = 0; i < constructionLayers.Count(); i++)
-                {
-                    if (!constructionLayers.ElementAt(i).Name.Equals(constructionLayers_TBD[i].Name) || !Core.Query.AlmostEqual(constructionLayers.ElementAt(i).Thickness, constructionLayers_TBD[i].Thickness))
-                    {
-                        update = true;
-                        break;
-                    }
-                }
-            }
+            //Commented code to solve issue when only one propertie of material is modified but name remain the same
+            //MD 20024.05.16
+            //if (constructionLayers_TBD != null && constructionLayers != null && constructionLayers_TBD.Count == constructionLayers.Count())
+            //{
+            //    update = false;
+            //    for (int i = 0; i < constructionLayers.Count(); i++)
+            //    {
+            //        if (!constructionLayers.ElementAt(i).Name.Equals(constructionLayers_TBD[i].Name) || !Core.Query.AlmostEqual(constructionLayers.ElementAt(i).Thickness, constructionLayers_TBD[i].Thickness))
+            //        {
+            //            update = true;
+            //            break;
+            //        }
+            //    }
+            //}
 
             if (update)
             {
