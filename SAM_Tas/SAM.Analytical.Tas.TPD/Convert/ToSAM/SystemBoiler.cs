66eb175b6ab3173753e2a6e59d258d0112183ab8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-12-10 12:28:57 +0100

    SystemBoiler Properties Added

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemBoiler.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemBoiler.cs
@@ -6,26 +6,34 @@ namespace SAM.Analytical.Tas.TPD
 {
     public static partial class Convert
     {
-        public static SystemBoiler ToSAM(this BoilerPlant bolierPlan)
+        public static SystemBoiler ToSAM(this BoilerPlant boilerPlant)
         {
-            if (bolierPlan == null)
+            if (boilerPlant == null)
             {
                 return null;
             }
 
-            dynamic @dynamic = bolierPlan;
+            dynamic @dynamic = boilerPlant;
 
             SystemBoiler result = new SystemBoiler(@dynamic.Name);
             Modify.SetReference(result, @dynamic.GUID);
             
             result.Description = dynamic.Description;
+            result.Setpoint = boilerPlant.Setpoint?.ToSAM();
+            result.Efficiency = boilerPlant?.Efficiency?.ToSAM();
+            result.Duty = boilerPlant.Duty?.ToSAM();
+            result.DesignTemperatureDiffrence = boilerPlant.DesignDeltaT;
+            result.Capacity = boilerPlant.Capacity;
+            result.DesignPressureDrop = boilerPlant.DesignPressureDrop;
+            result.AncillaryLoad = boilerPlant.AncillaryLoad?.ToSAM();
+            result.LossesInSizing = boilerPlant.LossesInSizing == 1;
 
             Point2D location = ((TasPosition)@dynamic.GetPosition())?.ToSAM();
 
             DisplaySystemBoiler displaySystemBoiler = Systems.Create.DisplayObject<DisplaySystemBoiler>(result, location, Systems.Query.DefaultDisplaySystemManager());
             if(displaySystemBoiler != null)
             {
-                ITransform2D transform2D = ((IPlantComponent)bolierPlan).Transform2D();
+                ITransform2D transform2D = ((IPlantComponent)boilerPlant).Transform2D();
                 if (transform2D != null)
                 {
                     displaySystemBoiler.Transform(transform2D);
