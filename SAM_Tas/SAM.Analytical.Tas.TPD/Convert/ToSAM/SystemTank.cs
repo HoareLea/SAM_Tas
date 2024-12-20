4c368ecb42b2f1e0a89cae558e007673884ddcbc
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-12-20 10:48:55 +0100

    SystemTank Properties Conversion Added

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemTank.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemTank.cs
@@ -19,6 +19,21 @@ namespace SAM.Analytical.Tas.TPD
             Modify.SetReference(result, @dynamic.GUID);
 
             result.Description = dynamic.Description;
+            result.InsulationConductivity = tank.InsConductivity;
+            result.InsulationThickness = tank.InsThickness;
+            result.Volume = tank.Volume;
+            result.HeatExchangeEfficiency1 = tank.HeatExEff1;
+            result.HeatExchangeEfficiency2 = tank.HeatExEff2;
+            result.Height = tank.Height;
+            result.AmbientTemperature = tank.AmbTemp?.ToSAM();
+            result.Setpoint = tank.Setpoint.ToSAM();
+            result.Capacity1 = tank.Capacity1;
+            result.Capacity2 = tank.Capacity2;
+            result.Capacity3 = tank.Capacity3;
+            result.DesignPressureDrop1 = tank.DesignPressureDrop1;
+            result.DesignPressureDrop2 = tank.DesignPressureDrop2;
+            result.DesignPressureDrop3 = tank.DesignPressureDrop3;
+            result.DefinedHeatLossRate = tank.DefinedHeatLoss;
 
             Point2D location = ((TasPosition)@dynamic.GetPosition())?.ToSAM();
 
