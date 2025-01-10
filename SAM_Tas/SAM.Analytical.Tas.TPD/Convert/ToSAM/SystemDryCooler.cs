14e7573606823cf79b325dd738deef807a80499f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-10 13:01:00 +0100

    SystemDryCooler Properties Added

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemDryCooler.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemDryCooler.cs
@@ -1,6 +1,7 @@
 ﻿using TPD;
 using SAM.Analytical.Systems;
 using SAM.Geometry.Planar;
+using System.Collections.Generic;
 
 namespace SAM.Analytical.Tas.TPD
 {
@@ -18,7 +19,45 @@ namespace SAM.Analytical.Tas.TPD
             SystemDryCooler result = new SystemDryCooler(@dynamic.Name);
             Modify.SetReference(result, @dynamic.GUID);
 
+            List<FuelSource> fuelSources = Query.FuelSources(dryCooler as PlantComponent);
+            if (fuelSources != null && fuelSources.Count > 0)
+            {
+                result.SetValue(Core.Systems.SystemObjectParameter.FanEnergySourceName, fuelSources[0]?.Name);
+                if (fuelSources.Count > 1)
+                {
+                    result.SetValue(Core.Systems.SystemObjectParameter.AncillaryEnergySourceName, fuelSources[1]?.Name);
+                }
+            }
+
             result.Description = dynamic.Description;
+
+            result.DesignPressureDrop = dynamic.DesignPressureDrop;
+            result.Capacity = dynamic.Capacity;
+            result.CoolingSetpoint = ((ProfileData)dynamic.CoolingSetpoint)?.ToSAM();
+            result.MaxFlowRate = ((SizedVariable)dynamic.MaxFlowRate)?.ToSAM();
+            result.FanSFP = ((ProfileData)dynamic.FanSFP)?.ToSAM();
+            result.DryCoolerExchangerCalculationMethod = ((tpdExchangerCalcMethod)dynamic.DryCoolerExchType).ToSAM();
+            result.Efficiency = ((ProfileData)dynamic.Efficiency)?.ToSAM();
+            result.HeatTransferSurfaceArea = dynamic.HeatTransSurfArea;
+            result.HeatTransferCoefficient = dynamic.HeatTransCoeff;
+            result.ExchangerType = ((tpdExchangerType)dynamic.ExchangerType).ToSAM();
+            result.AllowHeating = dynamic.AllowHeating;
+            result.HeatingSetpoint = ((ProfileData)dynamic.HeatingSetpoint)?.ToSAM();
+            result.MinSetpointTemperatureDifferenceCooling = dynamic.MinSetPDeltaTCooling;
+            result.MinSetpointTemperatureDifferenceHeating = dynamic.MinSetPDeltaTHeating;
+            result.HasPreCooling = dynamic.HasPreCooling;
+            result.PreCoolingEffectiveness = ((ProfileData)dynamic.PreCoolEffectiveness)?.ToSAM();
+            result.AncillaryLoad = ((ProfileData)dynamic.AncillaryLoad)?.ToSAM();
+            result.PreCoolingWaterFlowCapacity = ((SizedVariable)dynamic.PreCoolWaterFlowCap)?.ToSAM();
+            result.MinAirFlowRate = dynamic.MinAirFlowRate;
+            result.MinAirFlowRatio = dynamic.MinAFRRatio;
+            result.VariableFans = dynamic.VariableFans;
+            result.DesignExternalDryBulbTemperature = dynamic.MinAirFlDesignExternalDBowRate;
+            result.DesignExternalDryBulbTemperatureSizingType = ((tpdSizedVariable)dynamic.DesignExternalDBSource).ToSAM();
+            result.LimitingDryBulbTemperature = dynamic.LimitingDB;
+            result.DesignRange = dynamic.DesignRange;
+            result.DesignWaterFlowRate = dynamic.DesignWaterFlowRate;
+            result.WaterFlowSizingType = ((tpdSizedVariable)dynamic.WaterFlowSizingType).ToSAM();
 
             Point2D location = ((TasPosition)@dynamic.GetPosition())?.ToSAM();
 
