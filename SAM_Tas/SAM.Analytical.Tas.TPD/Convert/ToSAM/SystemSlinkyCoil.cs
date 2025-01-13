667f4a27279963522640bdc563cd35ca301b7e5e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-13 11:38:10 +0100

    SystemSlinkyCoil Properties Added

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemSlinkyCoil.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemSlinkyCoil.cs
@@ -20,6 +20,26 @@ namespace SAM.Analytical.Tas.TPD
 
             result.Description = dynamic.Description;
 
+            result.DesignPressureDrop = dynamic.DesignPressureDrop;
+            result.Capacity = dynamic.Capacity;
+            result.GroundDensity = dynamic.GroundDensity;
+            result.GroundHeatCapacity = dynamic.GroundHeatCap;
+            result.GroundConductivity = dynamic.GroundConductivity;
+            result.GroundSolarReflectance = dynamic.GroundSolarReflectance;
+            result.InsidePipeDiameter = dynamic.PipeDiamIn;
+            result.OutsidePipeDiameter = dynamic.PipeDiamOut;
+            result.PipeConductivity = dynamic.PipeConductivity;
+            result.LoopPitch = dynamic.LoopPitch;
+            result.LoopWidth = dynamic.LoopWidth;
+            result.LoopHeight = dynamic.LoopHeight;
+            result.IsUprightCoil = dynamic.IsUprightCoil;
+            result.FillDensity = dynamic.FillDensity;
+            result.FillHeatCapacity = dynamic.FillHeatCap;
+            result.FillConductivity = dynamic.FillConductivity;
+            result.TrenchLength = dynamic.TrenchLength;
+            result.TrenchDepth = dynamic.TrenchDepth;
+            result.TrenchWidth = dynamic.TrenchWidth;
+
             Point2D location = ((TasPosition)@dynamic.GetPosition())?.ToSAM();
 
             DisplaySystemSlinkyCoil displaySystemSlinkyCoil = Systems.Create.DisplayObject<DisplaySystemSlinkyCoil>(result, location, Systems.Query.DefaultDisplaySystemManager());
