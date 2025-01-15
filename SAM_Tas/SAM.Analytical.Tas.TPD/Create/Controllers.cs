ed2625d16f42a1743b099b95763debd45fb7c863
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-15 17:37:11 +0100

    Controllers ControlArcs Added (WIP)

--- SAM_Tas/SAM.Analytical.Tas.TPD/Create/Controllers.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Create/Controllers.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using SAM.Core.Systems;
 using SAM.Analytical.Systems;
+using SAM.Geometry.Systems;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace SAM.Analytical.Tas.TPD
 {
@@ -15,21 +17,92 @@ namespace SAM.Analytical.Tas.TPD
                 return null;
             }
 
-            List<Controller> result = new List<Controller>();
+            List<Tuple<Controller, IDisplaySystemController>> tuples = new List<Tuple<Controller, IDisplaySystemController>>();
 
             List<IDisplaySystemController> displaySystemControllers = systemPlantRoom.GetSystemComponents<IDisplaySystemController>(airSystem);
             foreach(IDisplaySystemController displaySystemController in displaySystemControllers)
             {
-                Controller plantController = displaySystemController.ToTPD(system);
-                if(plantController == null)
+                Controller controller = displaySystemController.ToTPD(system);
+                if(controller == null)
                 {
                     continue;
                 }
 
-                result.Add(plantController);
+
+                tuples.Add(new Tuple<Controller, IDisplaySystemController>(controller, displaySystemController));
             }
 
-            return result;
+            foreach (Tuple<Controller, IDisplaySystemController> tuple in tuples)
+            {
+                List<IDisplaySystemController> displaySystemControllers_Connected = systemPlantRoom.GetRelatedObjects<IDisplaySystemController>(tuple.Item2);
+                if (displaySystemControllers_Connected != null)
+                {
+                    foreach (IDisplaySystemController displaySystemController_Connected in displaySystemControllers_Connected)
+                    {
+                        Controller controller_Connected = tuples.Find(x => x.Item2.Guid == displaySystemController_Connected.Guid)?.Item1;
+
+                        ControlArc controlArc = tuple.Item1.AddChainArc(controller_Connected);
+
+                        List<ISystemConnection> systemConnections = systemPlantRoom.GetSystemConnections(tuple.Item2, displaySystemController_Connected, new SystemType(airSystem));
+                        if(systemConnections != null && systemConnections.Count > 0)
+                        {
+                            DisplaySystemConnection displaySystemConnection = systemConnections.Find(x => x is DisplaySystemConnection) as DisplaySystemConnection;
+                            if (displaySystemConnection != null)
+                            {
+                                SystemPolyline systemPolyline = displaySystemConnection.SystemGeometry;
+                                List<Geometry.Planar.Point2D> point2Ds = systemPolyline.GetPoints().ConvertAll(x => x.ToTPD());
+                                for (int i = 1; i < point2Ds.Count - 1; i++)
+                                {
+                                    controlArc.AddNode(System.Convert.ToInt32(point2Ds[i].X), System.Convert.ToInt32(point2Ds[i].Y));
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if(dictionary != null)
+                {
+                    List<Core.Systems.SystemComponent> systemComponents_Connected = systemPlantRoom.GetRelatedObjects<Core.Systems.SystemComponent>(tuple.Item2);
+                    if (systemComponents_Connected != null)
+                    {
+                        foreach (Core.Systems.SystemComponent systemComponent_Connected in systemComponents_Connected)
+                        {
+                            if(!dictionary.TryGetValue(systemComponent_Connected.Guid, out global::TPD.ISystemComponent systemComponent_TPD) || systemComponent_TPD == null)
+                            {
+                                continue;
+                            }
+
+                            ControlArc controlArc = tuple.Item1.AddControlArc(systemComponent_TPD as global::TPD.SystemComponent);
+
+                            List<ISystemConnection> systemConnections = systemPlantRoom.GetSystemConnections(tuple.Item2, systemComponent_Connected, new SystemType(airSystem));
+                            if (systemConnections != null && systemConnections.Count > 0)
+                            {
+                                DisplaySystemConnection displaySystemConnection = systemConnections.Find(x => x is DisplaySystemConnection) as DisplaySystemConnection;
+                                if (displaySystemConnection != null)
+                                {
+                                    SystemPolyline systemPolyline = displaySystemConnection.SystemGeometry;
+                                    List<Geometry.Planar.Point2D> point2Ds = systemPolyline.GetPoints().ConvertAll(x => x.ToTPD());
+                                    for (int i = 1; i < point2Ds.Count - 1; i++)
+                                    {
+                                        controlArc.AddNode(System.Convert.ToInt32(point2Ds[i].X), System.Convert.ToInt32(point2Ds[i].Y));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                List<ISystemSensor> systemSensors_Connected = systemPlantRoom.GetRelatedObjects<ISystemSensor>(tuple.Item2);
+                if(systemSensors_Connected != null)
+                {
+                    foreach (ISystemSensor systemSensor_Connected in systemSensors_Connected)
+                    {
+
+                    }
+                }
+            }
+
+            return tuples.ConvertAll(x => x.Item1);
         }
 
     }
