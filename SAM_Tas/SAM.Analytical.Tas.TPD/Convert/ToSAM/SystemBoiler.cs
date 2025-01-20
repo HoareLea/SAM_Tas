61c43e28b5c12857733d737f28fe389e7458b204
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-20 13:15:51 +0100

    SystemBoiler Conversion Updated

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemBoiler.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemBoiler.cs
@@ -16,7 +16,22 @@ namespace SAM.Analytical.Tas.TPD
 
             dynamic @dynamic = boilerPlant;
 
-            SystemBoiler result = new SystemBoiler(@dynamic.Name);
+            SystemBoiler result = new SystemBoiler(@dynamic.Name)
+            {
+                DesignPressureDrop = @dynamic.DesignPressureDrop,
+                Description = dynamic.Description,
+                Setpoint = ((ProfileData)dynamic.Setpoint)?.ToSAM(),
+                Efficiency = ((ProfileData)dynamic.Efficiency)?.ToSAM(),
+                Duty = ((SizedVariable)dynamic.Duty)?.ToSAM(),
+                DesignTemperatureDifference = dynamic.DesignDeltaT,
+                Capacity = dynamic.Capacity,
+                AncillaryLoad = ((ProfileData)dynamic.AncillaryLoad)?.ToSAM(),
+                LossesInSizing = dynamic.LossesInSizing,
+            };
+
+            tpdBoilerPlantFlags tpdBoilerPlantFlags = (tpdBoilerPlantFlags)boilerPlant.Flags;
+            result.IsDomesticHotWater = tpdBoilerPlantFlags.HasFlag(tpdBoilerPlantFlags.tpdBoilerPlantIsDHW);
+
             Modify.SetReference(result, @dynamic.GUID);
 
             List<FuelSource> fuelSources = Query.FuelSources(boilerPlant as PlantComponent);
@@ -28,18 +43,6 @@ namespace SAM.Analytical.Tas.TPD
                     result.SetValue(Core.Systems.SystemObjectParameter.AncillaryEnergySourceName, fuelSources[1].Name);
                 }
             }
-
-
-            result.DesignPressureDrop = @dynamic.DesignPressureDrop;
-            result.Description = dynamic.Description;
-            result.Setpoint = ((ProfileData)dynamic.Setpoint)?.ToSAM();
-            result.Efficiency = ((ProfileData)dynamic.Efficiency)?.ToSAM();
-            result.Duty = ((SizedVariable)dynamic.Duty)?.ToSAM();
-            result.DesignTemperatureDifference = dynamic.DesignDeltaT;
-            result.Capacity = dynamic.Capacity;
-            result.DesignPressureDrop = dynamic.DesignPressureDrop;
-            result.AncillaryLoad = ((ProfileData)dynamic.AncillaryLoad)?.ToSAM();
-            result.LossesInSizing = dynamic.LossesInSizing;
 
             Point2D location = ((TasPosition)@dynamic.GetPosition())?.ToSAM();
 
