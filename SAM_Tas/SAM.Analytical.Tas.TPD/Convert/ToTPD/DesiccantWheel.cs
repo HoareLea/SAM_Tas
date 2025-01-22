44e2456bea65a168ca59c48870a49df1bcdfb37f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-22 17:08:09 +0100

    SystemDesiccantWheel conversion updated

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/DesiccantWheel.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/DesiccantWheel.cs
@@ -12,14 +12,24 @@ namespace SAM.Analytical.Tas.TPD
                 return null;
             }
 
-            dynamic result = system.AddDesiccantWheel();
-            //result.ExchLatType = tpdExchangerLatentType.tpdExchangerLatentHumRat;
-            //result.Setpoint.Value = 14;
-            //result.Flags = tpdExchangerFlags.tpdExchangerFlagAdjustForOptimiser;
+            DesiccantWheel result = system.AddDesiccantWheel();
+
+            dynamic @dynamic = result;
+            @dynamic.Name = displaySystemDesiccantWheel.Name;
+            @dynamic.Description = displaySystemDesiccantWheel.Description;
+
+            result.SensibleEfficiency?.Update(displaySystemDesiccantWheel.SensibleEfficiency);
+            result.Reactivation?.Update(displaySystemDesiccantWheel.Reactivation);
+            result.MinimumRH?.Update(displaySystemDesiccantWheel.MinimumRH);
+            result.MaximumRH?.Update(displaySystemDesiccantWheel.MaximumRH);
+            result.SensibleHEEfficiency?.Update(displaySystemDesiccantWheel.SensibleHEEfficiency);
+            result.HESetpointMethod = displaySystemDesiccantWheel.HESetpointMethod.ToTPD();
+            result.HESetpoint?.Update(displaySystemDesiccantWheel.HESetpoint);
+            result.ElectricalLoad?.Update(displaySystemDesiccantWheel.ElectricalLoad);
 
             displaySystemDesiccantWheel.SetLocation(result as SystemComponent);
 
-            return result as DesiccantWheel;
+            return result;
         }
     }
 }
---
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/tpdSetpointMethod.cs
@@ -0,0 +1,23 @@
+﻿using SAM.Analytical.Systems;
+using TPD;
+
+namespace SAM.Analytical.Tas.TPD
+{
+    public static partial class Convert
+    {
+        public static tpdSetpointMethod ToTPD(this SetpointMode setpointMode)
+        {
+            switch (setpointMode)
+            {
+                case SetpointMode.On:
+                    return tpdSetpointMethod.tpdSetpointOn;
+
+                case SetpointMode.None:
+                    return tpdSetpointMethod.tpdSetpointNone;
+
+            }
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
