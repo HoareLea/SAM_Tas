5c9d880a3e4a52add95cf7c73f5a41a0df1e2c77
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-23 17:01:37 +0100

    PipeLossComponent properties added

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/PipeLossComponent.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/PipeLossComponent.cs
@@ -12,13 +12,30 @@ namespace SAM.Analytical.Tas.TPD
                 return null;
             }
 
-            dynamic result = plantRoom.AddPipeLossComponent();
-            result.Name = displaySystemPipeLossComponent.Name;
-            result.Description = displaySystemPipeLossComponent.Description;
+            PipeLossComponent result = plantRoom.AddPipeLossComponent();
+
+            dynamic @dynamic = result;
+            @dynamic.Name = displaySystemPipeLossComponent.Name;
+            @dynamic.Description = displaySystemPipeLossComponent.Description;
+
+            result.DesignPressureDrop = displaySystemPipeLossComponent.DesignPressureDrop;
+            result.Capacity = displaySystemPipeLossComponent.Capacity;
+            result.length = displaySystemPipeLossComponent.Length;
+            result.PipeDiamIn = displaySystemPipeLossComponent.InsidePipeDiameter;
+            result.PipeDiamOut = displaySystemPipeLossComponent.OutsidePipeDiameter;
+            result.PipeConductivity = displaySystemPipeLossComponent.PipeConductivity;
+            result.InsThickness = displaySystemPipeLossComponent.InsulationThickness;
+            result.InsConductivity = displaySystemPipeLossComponent.InsulationConductivity;
+            result.AmbTemp?.Update(displaySystemPipeLossComponent.AmbientTemperature);
+            result.IsUnderground = displaySystemPipeLossComponent.IsUnderground.ToTPD();
+            result.GrConductivity = displaySystemPipeLossComponent.GroundConductivity;
+            result.GrHeatCapacity = displaySystemPipeLossComponent.GroundHeatCapacity;
+            result.GrDensity = displaySystemPipeLossComponent.GroundDensity;
+            result.GrTemp = displaySystemPipeLossComponent.GroundTemperature;
 
             displaySystemPipeLossComponent.SetLocation(result as PlantComponent);
 
-            return result as PipeLossComponent;
+            return result;
         }
     }
 }
