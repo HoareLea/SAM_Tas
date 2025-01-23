66d929e032e9b0914e144a2924ceeaa6bc49f83f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-23 11:12:35 +0100

    Exchanger  parameter conversion updated

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/Exchanger.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/Exchanger.cs
@@ -12,16 +12,30 @@ namespace SAM.Analytical.Tas.TPD
                 return null;
             }
 
-            dynamic result = system.AddExchanger();
-            result.ExchLatType = tpdExchangerLatentType.tpdExchangerLatentHumRat;
-            result.LatentEfficiency.Value = displaySystemExchanger.LatentEfficiency;
-            result.SensibleEfficiency.Value = displaySystemExchanger.SensibleEfficiency;
-            result.Setpoint.Value = 14;
-            result.Flags = tpdExchangerFlags.tpdExchangerFlagAdjustForOptimiser;
+            Exchanger result = system.AddExchanger();
+
+            dynamic @dynamic = result;
+            result.ExchLatType = displaySystemExchanger.ExchangerLatentType.ToTPD();
+            result.ExchangerType = displaySystemExchanger.ExchangerType.ToTPD();
+            result.SensibleEfficiency?.Update(displaySystemExchanger.SensibleEfficiency);
+            result.HeatTransSurfArea = displaySystemExchanger.HeatTransferSurfaceArea;
+            result.HeatTransCoeff = displaySystemExchanger.HeatTransferCoefficient;
+            result.ExchLatType = displaySystemExchanger.ExchangerLatentType.ToTPD();
+            result.LatentEfficiency?.Update(displaySystemExchanger.LatentEfficiency);
+            result.SetpointMethod = displaySystemExchanger.SetpointMode.ToTPD();
+            result.Setpoint?.Update(displaySystemExchanger.Setpoint);
+            result.ElectricalLoad?.Update(displaySystemExchanger.ElectricalLoad);
+            result.Duty?.Update(displaySystemExchanger.Duty);
+            result.BypassFactor?.Update(displaySystemExchanger.BypassFactor);
+
+            //result.LatentEfficiency.Value = displaySystemExchanger.LatentEfficiency;
+            //result.SensibleEfficiency.Value = displaySystemExchanger.SensibleEfficiency;
+            //result.Setpoint.Value = 14;
+            //result.Flags = tpdExchangerFlags.tpdExchangerFlagAdjustForOptimiser;
 
             displaySystemExchanger.SetLocation(result as SystemComponent);
 
-            return result as Exchanger;
+            return result;
         }
     }
 }
---
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/tpdExchangerLatentType.cs
@@ -0,0 +1,23 @@
+﻿using SAM.Analytical.Systems;
+using TPD;
+
+namespace SAM.Analytical.Tas.TPD
+{
+    public static partial class Convert
+    {
+        public static tpdExchangerLatentType ToTPD(this ExchangerLatentType exchangerLatentType)
+        {
+            switch (exchangerLatentType)
+            {
+                case ExchangerLatentType.Enthalpy:
+                    return tpdExchangerLatentType.tpdExchangerLatentEnthalpy;
+
+                case ExchangerLatentType.HumidityRatio:
+                    return tpdExchangerLatentType.tpdExchangerLatentHumRat;
+
+            }
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
