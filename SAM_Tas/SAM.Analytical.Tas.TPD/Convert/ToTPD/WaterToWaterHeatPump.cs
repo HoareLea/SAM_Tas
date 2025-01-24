a76497de3b535d0aeac07328a057f206f9aa0c78
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-01-24 12:10:51 +0100

    WaterToWaterHeatPump properties conversion added

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/WaterToWaterHeatPump.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/WaterToWaterHeatPump.cs
@@ -12,13 +12,39 @@ namespace SAM.Analytical.Tas.TPD
                 return null;
             }
 
-            dynamic result = plantRoom.AddWaterToWaterHeatPump();
-            result.Name = displaySystemWaterToWaterHeatPump.Name;
-            result.Description = displaySystemWaterToWaterHeatPump.Description;
+            WaterToWaterHeatPump result = plantRoom.AddWaterToWaterHeatPump();
+
+            dynamic @dynamic = result;
+            @dynamic.Name = displaySystemWaterToWaterHeatPump.Name;
+            @dynamic.Description = displaySystemWaterToWaterHeatPump.Description;
+
+            result.HeatingSetpoint?.Update(displaySystemWaterToWaterHeatPump.HeatingSetpoint);
+            result.CoolingSetpoint?.Update(displaySystemWaterToWaterHeatPump.CoolingSetpoint);
+            result.HeatingEfficiency?.Update(displaySystemWaterToWaterHeatPump.HeatingEfficiency);
+            result.CoolingEfficiency?.Update(displaySystemWaterToWaterHeatPump?.CoolingEfficiency);
+            result.HeatingDuty?.Update(displaySystemWaterToWaterHeatPump.HeatingDuty);
+            result.CoolingDuty?.Update(displaySystemWaterToWaterHeatPump.CoolingDuty);
+            result.Capacity1 = displaySystemWaterToWaterHeatPump.Capacity1;
+            result.DesignPressureDrop1 = displaySystemWaterToWaterHeatPump.DesignPressureDrop1;
+            result.Capacity2 = displaySystemWaterToWaterHeatPump.Capacity2;
+            result.DesignPressureDrop2 = displaySystemWaterToWaterHeatPump.DesignPressureDrop2;
+            result.DesignDeltaT1 = displaySystemWaterToWaterHeatPump.DesignTemperatureDifference1;
+            result.DesignDeltaT2 = displaySystemWaterToWaterHeatPump.DesignTemperatureDifference2;
+            result.MotorEfficiency?.Update(displaySystemWaterToWaterHeatPump.MotorEfficiency);
+            result.AncillaryLoad?.Update(displaySystemWaterToWaterHeatPump.AncillaryLoad);
+            result.LossesInSizing = displaySystemWaterToWaterHeatPump.LossesInSizing.ToTPD();
+
+            if (displaySystemWaterToWaterHeatPump.LossesInSizing || displaySystemWaterToWaterHeatPump.IsDomesticHotWater)
+            {
+                tpdWaterToWaterHeatPumpFlags tpdWaterToWaterHeatPumpFlags = displaySystemWaterToWaterHeatPump.LossesInSizing && displaySystemWaterToWaterHeatPump.IsDomesticHotWater ? tpdWaterToWaterHeatPumpFlags.tpdWaterToWaterHeatPumpLossesInSizing | tpdWaterToWaterHeatPumpFlags.tpdWaterToWaterHeatPumpIsDHW :
+                    displaySystemWaterToWaterHeatPump.LossesInSizing ? tpdWaterToWaterHeatPumpFlags.tpdWaterToWaterHeatPumpLossesInSizing : tpdWaterToWaterHeatPumpFlags.tpdWaterToWaterHeatPumpIsDHW;
+
+                result.Flags = (int)tpdWaterToWaterHeatPumpFlags;
+            }
 
             displaySystemWaterToWaterHeatPump.SetLocation(result as PlantComponent);
 
-            return result as WaterToWaterHeatPump;
+            return result;
         }
     }
 }
