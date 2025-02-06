f4ef4b7d5979e81eb77cfd3e5a2ac3827793950c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-06 11:32:34 +0100

    SystemEnergySource initial conversion

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemEnergySource.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToSAM/SystemEnergySource.cs
@@ -25,6 +25,60 @@ namespace SAM.Analytical.Tas.TPD
                 result = new SystemEnergySource(fuelSource.Name);
             }
 
+            result.Description = fuelSource.Description;
+            result.ScheduleName = fuelSource.Schedule?.Name;
+            result.CustomerMonthlyCharge = fuelSource.CustomerMonthlyCharge;
+            result.FuelCostAdjustment = fuelSource.FuelCostAdjustment;
+            result.Discount = fuelSource.Discount;
+
+            result.PeakCost = new Core.IndexedDoubles();
+            switch (fuelSource.TimeOfUseType)
+            {
+                case tpdTimeOfUseType.tpdTimeOfUseValue:
+                    result.PeakCost.Add(0, fuelSource.PeakCost);
+                    break;
+
+                case tpdTimeOfUseType.tpdTimeOfUseHourly:
+                    
+                    break;
+
+                case tpdTimeOfUseType.tpdTimeOfUseYearly:
+
+                    break;
+            }
+
+            result.CO2Factor = new Core.IndexedDoubles();
+            switch (fuelSource.CO2TimeOfUseType)
+            {
+                case tpdTimeOfUseType.tpdTimeOfUseValue:
+                    result.CO2Factor.Add(0, fuelSource.CO2Factor);
+                    break;
+
+                case tpdTimeOfUseType.tpdTimeOfUseHourly:
+
+                    break;
+
+                case tpdTimeOfUseType.tpdTimeOfUseYearly:
+
+                    break;
+            }
+
+            result.PrimaryEnergyFactor = new Core.IndexedDoubles();
+            switch (fuelSource.PEFTimeOfUseType)
+            {
+                case tpdTimeOfUseType.tpdTimeOfUseValue:
+                    result.PrimaryEnergyFactor.Add(0, fuelSource.PrimaryEnergyFactor);
+                    break;
+
+                case tpdTimeOfUseType.tpdTimeOfUseHourly:
+
+                    break;
+
+                case tpdTimeOfUseType.tpdTimeOfUseYearly:
+
+                    break;
+            }
+
             return result;
         }
     }
