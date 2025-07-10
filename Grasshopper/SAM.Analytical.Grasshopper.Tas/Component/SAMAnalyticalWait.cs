363f9c661a9df236f0ffefa312ea6a757c8ac80e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-07-10 16:47:26 +0200

    Progress bar updates (WIP)

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Tas/Component/SAMAnalyticalWait.cs
@@ -0,0 +1,119 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Tas.Properties;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper.Tas
+{
+    public class SAMAnalyticalWait : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("84956c11-9b8f-4e7c-ba24-a78755d7f7e3");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small3;
+
+
+        public override GH_Exposure Exposure => GH_Exposure.tertiary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalWait()
+          : base("SAMAnalytical.Wait", "SAMAnalytical.Wait",
+              "SAM Analytical Wait",
+              "SAM WIP", "Test")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                global::Grasshopper.Kernel.Parameters.Param_Integer @int = null;
+
+                @int = new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "_count_", NickName = "_count", Description = "Count", Access = GH_ParamAccess.item };
+                @int.SetPersistentData(100);
+                result.Add(new GH_SAMParam(@int, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Boolean @boolean = null;
+
+                @boolean = new global::Grasshopper.Kernel.Parameters.Param_Boolean() { Name = "_run", NickName = "_run", Description = "Connect a boolean toggle to run.", Access = GH_ParamAccess.item };
+                @boolean.SetPersistentData(false);
+                result.Add(new GH_SAMParam(@boolean, ParamVisibility.Binding));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Boolean() { Name = "successful", NickName = "successful", Description = "Correctly imported?", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index_Successful = Params.IndexOfOutputParam("successful");
+            if (index_Successful != -1)
+            {
+                dataAccess.SetData(index_Successful, false);
+            }
+
+            int index;
+
+            bool run = false;
+            index = Params.IndexOfInputParam("_run");
+            if (index == -1 || !dataAccess.GetData(index, ref run))
+                run = false;
+
+            if (!run)
+                return;
+
+            int count = 100;
+            index = Params.IndexOfInputParam("_count_");
+            if (index == -1 || !dataAccess.GetData(index, ref count))
+                count = 100;
+
+            using (Core.Windows.Forms.ProgressForm progressForm = new Core.Windows.Forms.ProgressForm("Wait", count))
+            {
+                for(int i=0; i < count; i++)
+                {
+                    progressForm.Update((i + 1).ToString());
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+
+            if (index_Successful != -1)
+            {
+                dataAccess.SetData(index_Successful, true);
+            }
+        }
+    }
+}
