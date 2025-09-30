c67dbd021e4767071eba1410e173d9f934581a1e
Michal Dengusiak <tassimulation@gmail.com>
2025-09-30 14:53:10 +0200

    Update RoomUse.cs

--- SAM_Tas/SAM.Analytical.Tas.TM59/Query/RoomUse.cs
+++ SAM_Tas/SAM.Analytical.Tas.TM59/Query/RoomUse.cs
@@ -11,20 +11,25 @@ namespace SAM.Analytical.Tas.TM59
                 return Tas.RoomUse.Undefined;
             }
 
-            List<TM59SpaceApplication> tM59SpaceApplications = tM59Manager.TM59SpaceApplications(space);
-            if(tM59SpaceApplications == null || tM59SpaceApplications.Count == 0)
+            List<TM59SpaceApplication> tM59SpaceApplications = tM59Manager.TM59SpaceApplications(space?.InternalCondition);
+            if (tM59SpaceApplications == null || tM59SpaceApplications.Count == 0)
             {
-                return Tas.RoomUse.Other;
+                tM59SpaceApplications = tM59Manager.TM59SpaceApplications(space);
             }
 
-            if(tM59SpaceApplications.Contains(TM59SpaceApplication.Living) || tM59SpaceApplications.Contains(TM59SpaceApplication.Cooking))
+            if (tM59SpaceApplications == null || tM59SpaceApplications.Count == 0)
             {
-                return Tas.RoomUse.LivingRoomOrKitchen;
+                return Tas.RoomUse.Other;
             }
 
             if (tM59SpaceApplications.Contains(TM59SpaceApplication.Sleeping))
             {
                 return Tas.RoomUse.Bedroom;
+            }
+
+            if (tM59SpaceApplications.Contains(TM59SpaceApplication.Living) || tM59SpaceApplications.Contains(TM59SpaceApplication.Cooking))
+            {
+                return Tas.RoomUse.LivingRoomOrKitchen;
             }
 
             return Tas.RoomUse.Other;
