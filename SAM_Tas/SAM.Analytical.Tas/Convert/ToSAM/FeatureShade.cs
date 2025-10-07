70caad185fe2c2a10f6bca5714ad2236c348fb90
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-10-07 11:29:06 +0200

    FeatureShade initial conversion methods added

---
+++ SAM_Tas/SAM.Analytical.Tas/Convert/ToSAM/FeatureShade.cs
@@ -0,0 +1,31 @@
+﻿using SAM.Core;
+
+
+namespace SAM.Analytical.Tas
+{
+    public static partial class Convert
+    {
+        public static FeatureShade ToSAM(this TBD.IFeatureShade featureShade)
+        {
+            if (featureShade == null)
+            {
+                return null;
+            }
+
+            return new FeatureShade(
+                featureShade.name, 
+                featureShade.description,
+                featureShade.surfaceHeight,
+                featureShade.surfaceWidth,
+                featureShade.leftFinDepth,
+                featureShade.leftFinOffset,
+                featureShade.leftFinTransmittance,
+                featureShade.rightFinDepth,
+                featureShade.rightFinOffset,
+                featureShade.rightFinTransmittance,
+                featureShade.overhangDepth,
+                featureShade.overhangOffset,
+                featureShade.overhangTransmittance);
+        }
+    }
+}
---
+++ SAM_Tas/SAM.Analytical.Tas/Convert/ToTBD/FeatureShade.cs
@@ -0,0 +1,31 @@
+﻿namespace SAM.Analytical.Tas
+{
+    public static partial class Convert
+    {
+
+        public static TBD.FeatureShade ToTBD(this FeatureShade featureShade)
+        {
+            if(featureShade == null)
+            {
+                return null;
+            }
+
+            TBD.FeatureShade result = new TBD.FeatureShade();
+            result.name = featureShade.Name;
+            result.description = featureShade.Description;
+            result.surfaceHeight = System.Convert.ToSingle(featureShade.SurfaceHeight);
+            result.surfaceWidth = System.Convert.ToSingle(featureShade.SurfaceWidth);
+            result.leftFinDepth = System.Convert.ToSingle(featureShade.LeftFinDepth);
+            result.leftFinOffset = System.Convert.ToSingle(featureShade.LeftFinOffset);
+            result.leftFinTransmittance = System.Convert.ToSingle(featureShade.LeftFinTransmittance);
+            result.rightFinDepth = System.Convert.ToSingle(featureShade.RightFinDepth);
+            result.rightFinOffset = System.Convert.ToSingle(featureShade.RightFinOffset);
+            result.rightFinTransmittance = System.Convert.ToSingle(featureShade.RightFinTransmittance);
+            result.overhangDepth = System.Convert.ToSingle(featureShade.OverhangDepth);
+            result.overhangOffset = System.Convert.ToSingle(featureShade.OverhangOffset);
+            result.overhangTransmittance = System.Convert.ToSingle(featureShade.OverhangTransmittance);
+
+            return result;
+        }
+    }
+}
