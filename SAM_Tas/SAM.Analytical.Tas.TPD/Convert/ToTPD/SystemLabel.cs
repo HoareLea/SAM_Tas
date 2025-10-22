84f7e9b2b8425b8fd347991a1136ac171353153d
Michal Dengusiak <tassimulation@gmail.com>
2025-10-22 11:54:36 +0200

    Update SystemLabel.cs

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/SystemLabel.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/SystemLabel.cs
@@ -12,7 +12,9 @@ namespace SAM.Analytical.Tas.TPD
                 return null;
             }
 
-            SystemLabel result =  systemComponet.AddLabel(systemLabel.Text);
+            dynamic @dynamic = systemComponet;
+
+            SystemLabel result = @dynamic.AddLabel(systemLabel.Text);
             result.Label = systemLabel.Text;
 
             if (systemLabel is Geometry.Systems.DisplaySystemLabel)
