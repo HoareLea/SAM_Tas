887d812603e0907f2d7d021a56bfb04b08615871
Michal Dengusiak <tassimulation@gmail.com>
2025-10-23 16:11:10 +0200

    clean

--- SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/FourPipeHeatPump.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Convert/ToTPD/FourPipeHeatPump.cs
@@ -24,22 +24,22 @@ namespace SAM.Analytical.Tas.TPD
 
             EnergyCentre energyCentre = plantRoom.GetEnergyCentre();
 
-            result.HeatingSetpoint?.Update(displaySystemFourPipeHeatPump.HeatingSetpoint, energyCentre);
-            result.CoolingSetpoint?.Update(displaySystemFourPipeHeatPump.CoolingSetpoint, energyCentre);
-            result.HeatingEfficiency?.Update(displaySystemFourPipeHeatPump.HeatingEfficiency, energyCentre);
-            result.CoolingEfficiency?.Update(displaySystemFourPipeHeatPump?.CoolingEfficiency, energyCentre);
-            result.HeatingDuty?.Update(displaySystemFourPipeHeatPump.HeatingDuty, plantRoom);
-            result.CoolingDuty?.Update(displaySystemFourPipeHeatPump.CoolingDuty, plantRoom);
-            result.Capacity1 = displaySystemFourPipeHeatPump.Capacity_1;
-            result.DesignPressureDrop1 = displaySystemFourPipeHeatPump.DesignPressureDrop_1;
-            result.Capacity2 = displaySystemFourPipeHeatPump.Capacity_2;
-            result.DesignPressureDrop2 = displaySystemFourPipeHeatPump.DesignPressureDrop_2;
-            result.DesignDeltaT1 = displaySystemFourPipeHeatPump.DesignTemperatureDifference_1;
-            result.DesignDeltaT2 = displaySystemFourPipeHeatPump.DesignTemperatureDifference_2;
-            result.MotorEfficiency?.Update(displaySystemFourPipeHeatPump.MotorEfficiency, energyCentre);
-            result.AncillaryLoad?.Update(displaySystemFourPipeHeatPump.AncillaryLoad, energyCentre);
-            result.ADFCoolMode = displaySystemFourPipeHeatPump.ADFCoolingMode;
-            result.ADFHeatMode = displaySystemFourPipeHeatPump.ADFHeatingMode;
+            @dynamic.HeatingSetpoint?.Update(displaySystemFourPipeHeatPump.HeatingSetpoint, energyCentre);
+            @dynamic.CoolingSetpoint?.Update(displaySystemFourPipeHeatPump.CoolingSetpoint, energyCentre);
+            @dynamic.HeatingEfficiency?.Update(displaySystemFourPipeHeatPump.HeatingEfficiency, energyCentre);
+            @dynamic.CoolingEfficiency?.Update(displaySystemFourPipeHeatPump?.CoolingEfficiency, energyCentre);
+            @dynamic.HeatingDuty?.Update(displaySystemFourPipeHeatPump.HeatingDuty, plantRoom);
+            @dynamic.CoolingDuty?.Update(displaySystemFourPipeHeatPump.CoolingDuty, plantRoom);
+            @dynamic.Capacity1 = displaySystemFourPipeHeatPump.Capacity_1;
+            @dynamic.DesignPressureDrop1 = displaySystemFourPipeHeatPump.DesignPressureDrop_1;
+            @dynamic.Capacity2 = displaySystemFourPipeHeatPump.Capacity_2;
+            @dynamic.DesignPressureDrop2 = displaySystemFourPipeHeatPump.DesignPressureDrop_2;
+            @dynamic.DesignDeltaT1 = displaySystemFourPipeHeatPump.DesignTemperatureDifference_1;
+            @dynamic.DesignDeltaT2 = displaySystemFourPipeHeatPump.DesignTemperatureDifference_2;
+            @dynamic.MotorEfficiency?.Update(displaySystemFourPipeHeatPump.MotorEfficiency, energyCentre);
+            @dynamic.AncillaryLoad?.Update(displaySystemFourPipeHeatPump.AncillaryLoad, energyCentre);
+            @dynamic.ADFCoolMode = displaySystemFourPipeHeatPump.ADFCoolingMode;
+            @dynamic.ADFHeatMode = displaySystemFourPipeHeatPump.ADFHeatingMode;
 
 
             FuelSource fuelSource;
