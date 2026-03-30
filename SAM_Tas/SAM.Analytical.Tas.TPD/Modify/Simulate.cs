208def9a6807392d88777b5db8897400ad0e4b36
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-03-30 16:56:18 +0200

    Headers added

--- SAM_Tas/SAM.Analytical.Tas.TPD/Modify/Simulate.cs
+++ SAM_Tas/SAM.Analytical.Tas.TPD/Modify/Simulate.cs
@@ -1,4 +1,7 @@
-﻿using SAM.Core.Tas;
+﻿// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (c) 2020–2026 Michal Dengusiak & Jakub Ziolkowski and contributors
+
+using SAM.Core.Tas;
 using System.IO;
 using TPD;
 
